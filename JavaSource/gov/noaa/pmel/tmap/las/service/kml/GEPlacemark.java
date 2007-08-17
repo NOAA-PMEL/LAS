324361febd03d237529344ec50f409194981bf02
Jing Li <jing.li@noaa.gov>
2007-08-17 18:47:27 +0000

    generate place marks for grid points when user selects XY view
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@1696 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/las/service/kml/GEPlacemark.java
+++ JavaSource/gov/noaa/pmel/tmap/las/service/kml/GEPlacemark.java
@@ -27,11 +27,28 @@ public class GEPlacemark{
     HashMap<String, String> initLASInfo;
     String dataset_ID;
     String var_ID;
-    String view;
+
+    String ferret_view;
+    String ferret_deg_min_sec;
+    String ferret_dep_axis_scale;
+    String ferret_expression;
+    String ferret_fill_type;
+    String ferret_interpolate_data;
+    String ferret_line_color;
+    String ferret_line_or_sym;
+    String ferret_line_thickness;
+    String ferret_margins;
+    String ferret_size;
+    String ferret_use_graticules;
+    String ferret_use_ref_map;
+
     String thi;
     String tlo;
     String zhi;
     String zlo;
+    String t_user;
+    String z_user;
+
     String dsIntervals;
 
     String base_URL;
@@ -54,7 +71,6 @@ public class GEPlacemark{
     private void init(){
         dataset_ID = initLASInfo.get("dsID");
         var_ID = initLASInfo.get("varID");
-        view   = initLASInfo.get("view");
         dsIntervals = initLASInfo.get("dsIntervals");
 
         if(dsIntervals.contains("t")){
@@ -66,6 +82,26 @@ public class GEPlacemark{
             zlo = initLASInfo.get("zlo");
             zhi = initLASInfo.get("zhi");
         }
+
+        ferret_view             = initLASInfo.get("ferret_view");
+        ferret_deg_min_sec      = initLASInfo.get("ferret_deg_min_sec");
+        ferret_dep_axis_scale   = initLASInfo.get("ferret_dep_axis_scale");
+        ferret_expression       = initLASInfo.get("ferret_expression");
+        ferret_fill_type        = initLASInfo.get("ferret_fill_type");
+        ferret_interpolate_data = initLASInfo.get("ferret_interpolate_data");
+        ferret_line_color       = initLASInfo.get("ferret_line_color");
+        ferret_line_or_sym      = initLASInfo.get("ferret_line_or_sym");
+        ferret_line_thickness   = initLASInfo.get("ferret_line_thickness");
+        ferret_margins          = initLASInfo.get("ferret_margins");
+        ferret_size             = initLASInfo.get("ferret_size");
+        ferret_use_graticules   = initLASInfo.get("ferret_use_graticules");
+        ferret_use_ref_map      = initLASInfo.get("ferret_use_ref_map");
+        
+        if(dsIntervals.equals("xyzt") && ferret_view.equals("xy")){
+            t_user = initLASInfo.get("t_user");
+            z_user = initLASInfo.get("z_user");
+        }
+
     }
     /**
      * check longitude and convert it to be in [-180,180]
@@ -139,21 +175,69 @@ public class GEPlacemark{
         description = "<description>"
                       + "<![CDATA[";
 
-        if(view.contains("t")){
+/*
+        if(ferret_view.contains("t")){
             description = description  + "<a href='"
-                      + make1DPlotRequest()
+                      + make1DPlotRequest("t")
                       + "'><h2>Time Series Plot</h2></a><br />";
         }
-        if(view.contains("z")){
+        if(ferret_view.contains("z")){
             description = description +  "<a href='"
-                      + make1DPlotRequest()
+                      + make1DPlotRequest("z")
                       + "'><h2>Vertical Profile Plot</h2></a><br />";
         }
+
+        if(ferret_view.equals("xy")){
+            description = description +  "<a href='"
+                      + make1DPlotRequest("t")
+                      + "'><h2>Vertical Profile Plot</h2></a><br />";
+        }
+*/
+        if(dsIntervals.equals("xyt")){
+            description = description  + "<a href='"
+                      + make1DPlotRequest("t",null,null,tlo,thi)
+                      + "'><h2>Time Series Plot</h2></a><br />";
+        }
+
+        if(dsIntervals.equals("xyz")){
+            description = description  + "<a href='"
+                      + make1DPlotRequest("z",zlo,zhi,null,null)
+                      + "'><h2>Vertical Profile Plot</h2></a><br />";
+        }
+
+        if(dsIntervals.equals("xyzt")){
+            if(ferret_view.equals("xy")){
+                //use user selected t, t_user
+                description = description +  "<a href='"
+                      + make1DPlotRequest("z",zlo,zhi,t_user,t_user)
+                      + "'><h2>Vertical Profile Plot</h2></a>(Time: " + t_user+")";
+
+                //use user selected z, z_user
+                description = description  + "<a href='"
+                      + make1DPlotRequest("t",z_user,z_user,tlo,thi)
+                      + "'><h2>Time Series Plot</h2></a>(Depth(m): " + z_user+")";
+            }
+
+            if(ferret_view.equals("xyz")){
+                description = description +  "<a href='"
+                      + make1DPlotRequest("z",zlo,zhi,tlo,thi)
+                      + "'><h2>Vertical Profile Plot</h2></a><br />";
+            }
+
+            if(ferret_view.equals("xyt")){
+                description = description  + "<a href='"
+                      + make1DPlotRequest("t",zlo,zhi,tlo,thi)
+                      + "'><h2>Time Series Plot</h2></a><br />";
+            }
+
+        }
+
         description = description +  "<br />]]>"
                       + "</description>";
     }
 
-    private String make1DPlotRequest( )throws IOException{
+    private String make1DPlotRequest(String ferret1DView, String z_lo, String z_hi, String t_lo, String t_hi )
+    throws IOException{
 
         LASUIRequest lasUIRequest = new LASUIRequest();
 
@@ -181,13 +265,15 @@ public class GEPlacemark{
             intervals.put("z",zi);
         }
 */
-        if(dsIntervals.contains("t")){
-            String[] ti = new String[] {tlo,thi};
+        //if(dsIntervals.contains("t")){
+        if(t_lo != null && t_hi != null){
+            String[] ti = new String[] {t_lo,t_hi};
             intervals.put("t",ti);
         }
 
-        if(dsIntervals.contains("z")){
-            String[] zi = new String[] {zlo,zhi};
+        //if(dsIntervals.contains("z")){
+        if(z_lo != null && z_hi != null){
+            String[] zi = new String[] {z_lo,z_hi};
             intervals.put("z",zi);
         }
 
@@ -197,25 +283,69 @@ public class GEPlacemark{
         lasUIRequest.setRegion(region);
 
         //set ferret properties
-        if(view.contains("t")){lasUIRequest.setProperty("ferret", "view", "t");}
-        if(view.contains("z")){lasUIRequest.setProperty("ferret", "view", "z");}
+        //if(ferret_view.contains("t")){lasUIRequest.setProperty("ferret", "view", "t");}
+        //if(ferret_view.contains("z")){lasUIRequest.setProperty("ferret", "view", "z");}
+
+        lasUIRequest.setProperty("ferret", "view", ferret1DView);
 
         lasUIRequest.setProperty("ferret", "format", "line");
-        lasUIRequest.setProperty("ferret", "deg_min_sec", "default");
         lasUIRequest.setProperty("ferret", "image_format", "default");
-        lasUIRequest.setProperty("ferret", "interpolate_data", "false");
-        lasUIRequest.setProperty("ferret", "line_color", "default");
-        lasUIRequest.setProperty("ferret", "line_or_sym", "default"); 
-        lasUIRequest.setProperty("ferret", "line_thickness", "default");
-        lasUIRequest.setProperty("ferret", "line_margins", "default");
-        lasUIRequest.setProperty("ferret", "size", "0.5");
-        lasUIRequest.setProperty("ferret", "use_graticules", "default");   
-        lasUIRequest.setProperty("ferret", "line_ref_map", "default");
+
+
+        if(ferret_deg_min_sec != null && ferret_deg_min_sec != ""){
+            lasUIRequest.setProperty("ferret", "deg_min_sec", ferret_deg_min_sec);
+        }
+
+        if(ferret_dep_axis_scale != null && ferret_dep_axis_scale != ""){
+            lasUIRequest.setProperty("ferret", "dep_axis_scale", ferret_dep_axis_scale);
+        }
+
+        if(ferret_expression != null && ferret_expression != ""){
+            lasUIRequest.setProperty("ferret", "expression", ferret_expression);
+        }
+
+        if(ferret_fill_type != null && ferret_fill_type != ""){
+            lasUIRequest.setProperty("ferret", "fill_type", ferret_fill_type);
+        }
+ 
+        if(ferret_interpolate_data != null && ferret_interpolate_data != ""){
+            lasUIRequest.setProperty("ferret", "interpolate_data", ferret_interpolate_data);
+        }
+
+        if(ferret_line_color != null && ferret_line_color != ""){
+            lasUIRequest.setProperty("ferret", "line_color", ferret_line_color);
+        }
+
+        if(ferret_line_or_sym != null && ferret_line_or_sym != ""){
+            lasUIRequest.setProperty("ferret", "line_or_sym", ferret_line_or_sym); 
+        }
+
+        if(ferret_line_thickness != null && ferret_line_thickness != ""){
+            lasUIRequest.setProperty("ferret", "line_thickness", ferret_line_thickness);
+        }
+
+        if(ferret_margins != null && ferret_margins != ""){
+            lasUIRequest.setProperty("ferret", "margins", ferret_margins);
+        }
+
+        if(ferret_size != null && ferret_size != ""){
+            lasUIRequest.setProperty("ferret", "size", ferret_size);
+        }else{
+            lasUIRequest.setProperty("ferret", "size", "0.5");
+        }
+
+        if(ferret_use_graticules != null && ferret_use_graticules != ""){
+            lasUIRequest.setProperty("ferret", "use_graticules", ferret_use_graticules);   
+        }
+
+        if(ferret_use_ref_map != null && ferret_use_ref_map != ""){
+            lasUIRequest.setProperty("ferret", "use_ref_map", ferret_use_ref_map);
+        }
 
         String serverURL = base_URL.replaceFirst("output","ProductServer");
         String lasReq = serverURL+".do?xml="
-                           + lasUIRequest.toEncodedURLString()
-                           + "&stream=true&stream_ID=plot_image";
+                           + lasUIRequest.toEncodedURLString();
+        //                   + "&stream=true&stream_ID=plot_image";
         return lasReq;
     }
 
