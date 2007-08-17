86247a83e0cbd1fa8c3293a0ef373c8d836baa32
Jing Li <jing.li@noaa.gov>
2007-08-17 18:47:35 +0000

    generate place marks for grid points when user selects XY view
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@1697 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/las/service/kml/KMLTool.java
+++ JavaSource/gov/noaa/pmel/tmap/las/service/kml/KMLTool.java
@@ -265,22 +265,55 @@ public class KMLTool extends TemplateTool  {
 
             Element dsIDElement = lasReqInfo.getElementByXPath("/las_req_info/dataset_id");
             dsID = dsIDElement.getText();
-
             Element varIDElement = lasReqInfo.getElementByXPath("/las_req_info/variable_id");
             varID = varIDElement.getText();
-
             Element viewElement = lasReqInfo.getElementByXPath("/las_req_info/ferret_view");
             String view = viewElement.getText();
-
             Element dsIntervalsElement = lasReqInfo.getElementByXPath("/las_req_info/data_intervals");
             String dsIntervals = dsIntervalsElement.getText();
-
             initLASReq.put("dsID", dsID);
             initLASReq.put("varID", varID);
-            initLASReq.put("view", view);
+            initLASReq.put("ferret_view", view);
             initLASReq.put("dsIntervals", dsIntervals);
 
+            Element dmsElement = lasReqInfo.getElementByXPath("/las_req_info/ferret_deg_min_sec");
+            if(dmsElement != null){initLASReq.put("ferret_deg_min_sec", dmsElement.getText());}
+
+            Element dasElement = lasReqInfo.getElementByXPath("/las_req_info/ferret_dep_axis_scale");
+            if(dasElement != null){initLASReq.put("ferret_dep_axis_scale", dasElement.getText());}
+
+            Element expElement = lasReqInfo.getElementByXPath("/las_req_info/ferret_expression");
+            if(expElement != null){initLASReq.put("ferret_expression", expElement.getText());}
+
+            Element typElement = lasReqInfo.getElementByXPath("/las_req_info/ferret_fill_type");
+            if(typElement != null){initLASReq.put("ferret_fill_type", typElement.getText());}
+ 
+            Element inpElement = lasReqInfo.getElementByXPath("/las_req_info/ferret_interpolate_data");
+            if(inpElement != null){initLASReq.put("ferret_interpolate_data", inpElement.getText());}
+
+            Element colElement = lasReqInfo.getElementByXPath("/las_req_info/ferret_line_color");
+            if(colElement != null){initLASReq.put("ferret_line_color", colElement.getText());}
+
+            Element symElement = lasReqInfo.getElementByXPath("/las_req_info/ferret_line_or_sym");
+            if(symElement != null){initLASReq.put("ferret_line_or_sym", symElement.getText());}
+
+            Element thkElement = lasReqInfo.getElementByXPath("/las_req_info/ferret_line_thickness");
+            if(thkElement != null){initLASReq.put("ferret_line_thickness", thkElement.getText());}
+
+            Element mrgElement = lasReqInfo.getElementByXPath("/las_req_info/ferret_margins");
+            if(mrgElement != null){initLASReq.put("ferret_margins", mrgElement.getText());}
+
+            Element sizElement = lasReqInfo.getElementByXPath("/las_req_info/ferret_size");
+            if(sizElement != null){initLASReq.put("ferret_size", sizElement.getText());}
+
+            Element grtElement = lasReqInfo.getElementByXPath("/las_req_info/ferret_use_graticules");
+            if(grtElement != null){initLASReq.put("ferret_use_graticules", grtElement.getText());}
+
+            Element refElement = lasReqInfo.getElementByXPath("/las_req_info/ferret_use_ref_map");
+            if(refElement != null){initLASReq.put("ferret_use_ref_map", refElement.getText());}
+
             if(dsIntervals.contains("z")){
+                //get Z range
                 Element zElement = lasReqInfo.getElementByXPath("/las_req_info/z_region");
                 Element zloElement = zElement.getChild("z_lo");
                 String zlo  = zloElement.getText();
@@ -288,9 +321,14 @@ public class KMLTool extends TemplateTool  {
                 String zhi  = zhiElement.getText();
                 initLASReq.put("zlo", zlo);
                 initLASReq.put("zhi", zhi);
+
+                //get user selected Z if there is one (dataset is XYZT and ferret_view is XY)
+                Element usrzElement = lasReqInfo.getElementByXPath("/las_req_info/z_user");
+                if(usrzElement != null){initLASReq.put("z_user", usrzElement.getText());}
             }
        
             if(dsIntervals.contains("t")){
+                //get t range
                 Element tElement = lasReqInfo.getElementByXPath("/las_req_info/t_region");
                 Element tloElement = tElement.getChild("t_lo");
                 String tlo  = tloElement.getText();
@@ -298,7 +336,14 @@ public class KMLTool extends TemplateTool  {
                 String thi  = thiElement.getText();
                 initLASReq.put("tlo", tlo);
                 initLASReq.put("thi", thi);
+                
+                //get user selected t if there is one (dataset is XYZT and ferret_view is XY)
+                Element usrtElement = lasReqInfo.getElementByXPath("/las_req_info/t_user");
+                if(usrtElement != null){
+                    initLASReq.put("t_user", usrtElement.getText());
+                }
             }
+
         } catch (Exception e){
             log.info("error while reading las request info: " + e.toString());
         }
