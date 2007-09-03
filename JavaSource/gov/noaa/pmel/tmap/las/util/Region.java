ce6d4f7821378a9fd0fba72284453d3718d113f9
Roland Schweitzer <Roland Schweitzer@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2007-09-03 15:25:10 +0000

    Add a getRegions service to return the list of named sub-regions on the globe.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@1819 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ JavaSource/gov/noaa/pmel/tmap/las/util/Region.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package gov.noaa.pmel.tmap.las.util;
+
+import org.jdom.Element;
+
+/**
+ * @author rhs
+ *
+ */
+public class Region extends Container {
+
+	/**
+	 * @param element
+	 */
+	public Region(Element element) {
+		super(element);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ JavaSource/gov/noaa/pmel/tmap/las/util/RegionInterface.java
@@ -0,0 +1,19 @@
+package gov.noaa.pmel.tmap.las.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public interface RegionInterface {
+    /* These are supplied by the Container class in my implementation. */
+    public abstract String getID();
+    public abstract void setID(String ID);
+    public abstract String getName();
+    public abstract void setName(String name);
+    public abstract HashMap<String, ArrayList<NameValuePair>> getProperties();
+    public abstract void setProperties(HashMap<String, ArrayList<NameValuePair>> properties);
+    public abstract ArrayList<NameValuePair> getAttributes();
+    public abstract void setAttributes(ArrayList<NameValuePair> attributes);
+    public abstract String toXML();
+    public abstract String getAttributeValue(String name);
+    public abstract String getValue();
+}
