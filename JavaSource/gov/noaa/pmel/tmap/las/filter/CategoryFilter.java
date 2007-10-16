025d1ef3363b5b17661189b56bb1ae69ce8c2fe0
Roland Schweitzer <Roland Schweitzer@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2007-10-16 19:42:03 +0000

    Some JavaDoc tweaks for the gov.noaa.pmel.tmap.las.filter package.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@2044 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/las/filter/CategoryFilter.java
+++ JavaSource/gov/noaa/pmel/tmap/las/filter/CategoryFilter.java
@@ -1,13 +1,35 @@
+/**
+ * This software is provided by NOAA for full, free and open release.  It is
+ * understood by the recipient/user that NOAA assumes no liability for any
+ * errors contained in the code.  Although this software is released without
+ * conditions or restrictions in its use, it is expected that appropriate
+ * credit be given to its author and to the National Oceanic and Atmospheric
+ * Administration should the software be included by the recipient as an
+ * element in other product development. 
+ */
 package gov.noaa.pmel.tmap.las.filter;
 
 import org.jdom.Element;
 import org.jdom.filter.Filter;
 
+/**
+ * This class builds a filter that will find categories by ID.
+ * @author rhs
+ *
+ */
 public class CategoryFilter implements Filter {
     String ID;
+    /**
+     * A constructor to build a filter for a particular ID.
+     * @param id the desired category ID.
+     */
     public CategoryFilter(String id) {
         ID = id;
     }
+    /**
+     * This is the required matches method that returns true if an Object should be selected by the filter.
+     * @param obj the object to be tested.
+     */
     public boolean matches(Object obj) {
         Element element=null;
         if (obj instanceof Element) {
--- JavaSource/gov/noaa/pmel/tmap/las/filter/FindOldPropertyFilter.java
+++ JavaSource/gov/noaa/pmel/tmap/las/filter/FindOldPropertyFilter.java
@@ -1,3 +1,12 @@
+/**
+ * This software is provided by NOAA for full, free and open release.  It is
+ * understood by the recipient/user that NOAA assumes no liability for any
+ * errors contained in the code.  Although this software is released without
+ * conditions or restrictions in its use, it is expected that appropriate
+ * credit be given to its author and to the National Oceanic and Atmospheric
+ * Administration should the software be included by the recipient as an
+ * element in other product development. 
+ */
 package gov.noaa.pmel.tmap.las.filter;
 
 import org.jdom.Element;
@@ -5,11 +14,13 @@ import org.jdom.filter.Filter;
 /**
  * A filter that helps locate properties that are stored in the "old" style:
  * <p>
+ * <pre>
  * &lt;properties&gt;<br>
  * &nbsp;&nbsp;&lt;GROUP&gt;<br>
  * &nbsp;&nbsp;&nbsp;&lt;NAME&gt;VALUE&lt;/NAME&gt;<br>
  * &nbsp;&nbsp;&lt;/GROUP&gt;<br>
  * &lt;properties&gt;<br> 
+ * </pre>
  * @author Roland Schweitzer
  *
  */
@@ -25,6 +36,7 @@ public class FindOldPropertyFilter implements Filter {
     }
     /**
      * The implementation of the matches method which returns true of the JDOM element has the same name as the property.
+     * @param obj the object to be tested
      */
     public boolean matches(Object node) {
         Element element=null;
