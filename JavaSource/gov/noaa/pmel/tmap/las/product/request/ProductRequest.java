5eebbe6d09631a9a46350bb5dcf37c4fd52ff6a7
Roland Schweitzer <Roland Schweitzer@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2007-11-06 20:59:41 +0000

    Need to set the dataset_url in two different places.  Grrrr.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@2181 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/las/product/request/ProductRequest.java
+++ JavaSource/gov/noaa/pmel/tmap/las/product/request/ProductRequest.java
@@ -426,7 +426,12 @@ public class ProductRequest {
                                 String value = dataset_attrs.get(name);
                                 data.setAttribute(new Attribute("dataset_"+name, value));
                             }
-
+                            
+                            String datasetURL = data.getAttributeValue("dataset_url");
+                            if ( datasetURL == null || datasetURL.equals("") ) {
+                            	data.setAttribute(new Attribute("dataset_url", lasConfig.getDataAccessURL(varXPath, false)));
+                            }
+                            
                             List attribs = lasConfig.getDataOjectAttributes(varXPath);
 
                             if ( attribs.size() > 0 ) {
