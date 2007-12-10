3015da6fad342a4ad5ce6b7eb338fcb973811e03
rhs <rhs@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2007-12-10 22:47:42 +0000

    Add the config file for the tabledap service.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@2357 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ JavaSource/gov/noaa/pmel/tmap/las/service/tabledap/LASTabledapBackendConfig.java
@@ -0,0 +1,32 @@
+package gov.noaa.pmel.tmap.las.jdom;
+
+import org.jdom.Element;
+import org.jdom.JDOMException;
+
+/**
+ * @author Roland Schweitzer
+ * @author Bob Simons (bob.simons@noaa.gov)
+ *
+ */
+public class LASTabledapBackendConfig extends LASDocument {
+    
+    /**
+     * @param name
+     * @return
+     * @throws JDOMException
+     */
+    public String getURL(String name) throws JDOMException  {
+        Element server = getElementByXPath("/tabledap_servers/tabledap[@name='"+name+"']");
+        if ( server == null ) {
+            return "";
+        } else {
+            String url = server.getAttributeValue("url");
+            if ( url == null ) {
+                return "";
+            } else {
+                return url;
+            }
+        }
+    }
+
+}
