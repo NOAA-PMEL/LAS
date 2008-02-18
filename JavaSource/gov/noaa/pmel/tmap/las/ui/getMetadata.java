794d079c3c3dd8966deb12e4e8bb5add6c778e3e
Roland Schweitzer <Roland Schweitzer@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2008-02-18 17:15:57 +0000

    Rearrange metadata service so it looks like all the other services (even though it's actually a LAS product).  Make necessary changes to struts-config.xml.in.  Addresses ticket #394.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@2543 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/las/ui/getMetadata.java
+++ JavaSource/gov/noaa/pmel/tmap/las/ui/getMetadata.java
@@ -1,6 +1,7 @@
-package gov.noaa.pmel.tmap.las.product.server;
+package gov.noaa.pmel.tmap.las.ui;
 
 import gov.noaa.pmel.tmap.las.jdom.LASUIRequest;
+import gov.noaa.pmel.tmap.las.product.server.LASAction;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,7 +10,7 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-public class MetadataAction extends LASAction {
+public class getMetadata extends LASAction {
 
 	/* (non-Javadoc)
 	 * @see org.apache.struts.action.Action#execute(org.apache.struts.action.ActionMapping, org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
