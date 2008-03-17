0636fb688816ad2fdb18fc3960c9270684da827e
Roland Schweitzer <roland.schweitzer@noaa.gov>
2008-03-17 22:11:59 +0000

    As per Jeremy's request added capability to get all operations for all view for a particular variable.
    getOperations.do?dsid=foo&varid=bar  (no view parameter).
    
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@2708 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/las/ui/getOperations.java
+++ JavaSource/gov/noaa/pmel/tmap/las/ui/getOperations.java
@@ -6,16 +6,14 @@ package gov.noaa.pmel.tmap.las.ui;
 
 import gov.noaa.pmel.tmap.las.jdom.LASConfig;
 import gov.noaa.pmel.tmap.las.product.server.LASConfigPlugIn;
-import gov.noaa.pmel.tmap.las.util.Category;
 import gov.noaa.pmel.tmap.las.util.ContainerComparator;
-import gov.noaa.pmel.tmap.las.util.NameValuePair;
 import gov.noaa.pmel.tmap.las.util.Operation;
-import gov.noaa.pmel.tmap.las.util.Option;
+import gov.noaa.pmel.tmap.las.util.View;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 
 import javax.servlet.http.HttpServletRequest;
@@ -23,14 +21,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.jdom.JDOMException;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.XML;
 
 /** 
  * MyEclipse Struts
@@ -56,32 +51,52 @@ public class getOperations extends ConfigService {
 	public ActionForward execute(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response) {
 		LASConfig lasConfig = (LASConfig)servlet.getServletContext().getAttribute(LASConfigPlugIn.LAS_CONFIG_KEY);
+		
 		String dsID = request.getParameter("dsid");
 		String varID = request.getParameter("varid");
 		String view = request.getParameter("view");
 		String format = request.getParameter("format");
-		String grid_type = "";
+		
 		if ( format == null ) {
 			format = "json";
 		}
 		log.info("Starting: getOperations.do?dsid="+dsID+"&varid="+varID+"&view="+view+".");
 		try {
-			
-			ArrayList<Operation> operations = lasConfig.getOperations(view, dsID, varID);
+			ArrayList<Operation> operations;
+			if ( view != null) {
+				operations = lasConfig.getOperations(view, dsID, varID);
 
-			if ( operations.size() <= 0 ) {
-				sendError(response, "operations", format, "No operations found.");
-				return null;
-			} else {
-				// Check to see if there's something in there.
-				Operation op = operations.get(0);
-				String name = op.getName();
-				if ( name == null || name.equals("") ) {
+				if ( operations.size() <= 0 ) {
 					sendError(response, "operations", format, "No operations found.");
 					return null;
+				} else {
+					// Check to see if there's something in there.
+					Operation op = operations.get(0);
+					String name = op.getName();
+					if ( name == null || name.equals("") ) {
+						sendError(response, "operations", format, "No operations found.");
+						return null;
+					}
 				}
+			} else {
+				operations = new ArrayList<Operation>();
+				ArrayList<View> views = lasConfig.getViewsByDatasetAndVariable(dsID, varID);
+				HashMap<String, Operation> allOps = new HashMap<String, Operation>();
+				for (Iterator viewIt = views.iterator(); viewIt.hasNext();) {
+					View aView = (View) viewIt.next();
+					ArrayList<Operation> ops = lasConfig.getOperations(aView.getValue(), dsID, varID);
+					for (Iterator opsIt = ops.iterator(); opsIt.hasNext();) {
+						Operation op = (Operation) opsIt.next();
+						String id = op.getID();
+						allOps.put(id, op);
+					}
+				}
+				for (Iterator idIt = allOps.keySet().iterator(); idIt.hasNext();) {
+					String id = (String) idIt.next();
+					operations.add(allOps.get(id));
+				}
+				
 			}
-			
 			Collections.sort(operations, new ContainerComparator("order", "name"));
 
 			PrintWriter respout = response.getWriter();
@@ -95,14 +110,16 @@ public class getOperations extends ConfigService {
 				log.debug(json_response.toString(3));
 				json_response.write(respout);      
 			}
+
 			// JDOMException, JSONException and IOException expected
 		} catch (Exception e) {
 			sendError(response, "operations", format, e.toString());
 		} 
+
 		log.info("Finished: getOperations.do?dsid="+dsID+"&varid="+varID+"&view="+view+".");
 		return null;
 	}
-	private JSONObject toJSON(ArrayList<Operation> operations, String string) throws JSONException {
+	private JSONObject toJSON(ArrayList<Operation> operations, String wrapper) throws JSONException {
 		JSONObject json_response = new JSONObject();
 		JSONObject operations_object = new JSONObject();
 		for (Iterator operIt = operations.iterator(); operIt.hasNext();) {
@@ -112,7 +129,16 @@ public class getOperations extends ConfigService {
 		}
 		operations_object.put("status", "ok");
 		operations_object.put("error", "");
-		json_response.put("operations", operations_object);
+		json_response.put(wrapper, operations_object);
 		return json_response;
+	}
+	private JSONObject toJSONwithoutWrapper(ArrayList<Operation> operations) throws JSONException {
+		JSONObject operations_object = new JSONObject();
+		for (Iterator operIt = operations.iterator(); operIt.hasNext();) {
+			Operation op = (Operation) operIt.next();
+			JSONObject operation = op.toJSON();        
+			operations_object.array_accumulate("operation", operation);
+		}
+		return operations_object;
 	}
 }
