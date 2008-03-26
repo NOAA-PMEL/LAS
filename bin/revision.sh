2f9514938ab55ee49a9cc907fd7fa3028fb5e0f9
rhs <rhs@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2008-03-26 22:13:12 +0000

    Get more revision information into the info page.  (Switched to a Bourne shell because it's easier to check the exit status and such.)
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@2772 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- bin/revision.sh
+++ bin/revision.sh
@@ -1,20 +1,27 @@
-#!/bin/csh
+#!/bin/sh
 #
-if ( ! -e JavaSource ) then
+if [ ! -d JavaSource ]; then
 echo "Must be run from LAS home."
-exit
-endif
-#
-echo -n '#set ($revision="' > WebContent/productserver/templates/revision.vm
-svnversion -n >> WebContent/productserver/templates/revision.vm
-echo '")' >> WebContent/productserver/templates/revision.vm
+exit 1
+fi
+REV=`svnversion -n`
+if [ "$?" -ne "0" ]; then
+       echo "Subversion not installed.  That is ok..."
+       exit 1
+fi
+echo -n "#set (\$revision=\"$REV\")" > WebContent/productserver/templates/revision.vm
 
 head -26 WebContent/productserver/templates/info.vm > WebContent/productserver/templates/info_tmp.vm
 
 echo '<h3>Subversion Info:</h3>' >> WebContent/productserver/templates/info_tmp.vm
 echo '<pre>' >> WebContent/productserver/templates/info_tmp.vm
 svn info >> WebContent/productserver/templates/info_tmp.vm
+if [ "$?" -ne "0" ]; then
+       echo "Subversion not installed.  That is ok..."
+       rm  WebContent/productserver/templates/info_tmp.vm
+       exit 1
+fi
+echo '</pre>' >> WebContent/productserver/templates/info_tmp.vm
 echo '</body>' >> WebContent/productserver/templates/info_tmp.vm
 echo '</html>' >> WebContent/productserver/templates/info_tmp.vm
-echo '</pre>' >> WebContent/productserver/templates/info_tmp.vm
 mv WebContent/productserver/templates/info_tmp.vm WebContent/productserver/templates/info.vm
