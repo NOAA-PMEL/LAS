0b95d24f9896784d44f3fd0cc54e7d8de1413169
Roland Schweitzer <Roland Schweitzer@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2008-08-01 19:46:53 +0000

    An stub entry point class for GWT.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@3195 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ JavaSource/gov/noaa/pmel/tmap/las/client/LASEntryPoint.java
@@ -0,0 +1,19 @@
+package gov.noaa.pmel.tmap.las.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.ServiceDefTarget;
+
+public class LASEntryPoint implements EntryPoint {
+    RPCServiceAsync rpcService;
+    public void onModuleLoad() {
+        rpcService = (RPCServiceAsync) GWT.create(RPCService.class);
+        ServiceDefTarget endpoint = (ServiceDefTarget) rpcService;
+        String moduleRelativeURL = GWT.getModuleBaseURL();
+        String moduleName = GWT.getModuleName();
+        moduleRelativeURL = moduleRelativeURL.substring(0,moduleRelativeURL.indexOf(moduleName)-1);
+        moduleRelativeURL = moduleRelativeURL.substring(0,moduleRelativeURL.lastIndexOf("/")+1);
+        String rpcURL = moduleRelativeURL + "rpc";
+        endpoint.setServiceEntryPoint(rpcURL);
+    }
+}
