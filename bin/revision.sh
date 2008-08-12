8efc3737dd37e82bb88ac0e6403b472515193567
Roland Schweitzer <roland.schweitzer@noaa.gov>
2008-08-12 21:03:15 +0000

    Fixes to the info page to give it the V7 chrome and remove the configure generated file "revision.vm".
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@3238 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- bin/revision.sh
+++ bin/revision.sh
@@ -11,17 +11,11 @@ if [ "$?" -ne "0" ]; then
 fi
 echo -n "#set (\$revision=\"$REV\")" > WebContent/productserver/templates/revision.vm
 
-head -26 WebContent/productserver/templates/info.vm > WebContent/productserver/templates/info_tmp.vm
-
-echo '<h3>Subversion Info:</h3>' >> WebContent/productserver/templates/info_tmp.vm
-echo '<pre>' >> WebContent/productserver/templates/info_tmp.vm
-svn info >> WebContent/productserver/templates/info_tmp.vm
+echo '<h3>Subversion Info:</h3>' > WebContent/productserver/templates/svn.vm
+echo '<pre>' >> WebContent/productserver/templates/svn.vm
+svn info >> WebContent/productserver/templates/svn.vm
 if [ "$?" -ne "0" ]; then
-       echo "Subversion not installed.  That is ok..."
-       rm  WebContent/productserver/templates/info_tmp.vm
+       echo "Subversion not installed.  That is ok..." >> WebContent/productserver/templates/info_tmp.vm
        exit 1
 fi
-echo '</pre>' >> WebContent/productserver/templates/info_tmp.vm
-echo '</body>' >> WebContent/productserver/templates/info_tmp.vm
-echo '</html>' >> WebContent/productserver/templates/info_tmp.vm
-mv WebContent/productserver/templates/info_tmp.vm WebContent/productserver/templates/info.vm
+echo '</pre>' >> WebContent/productserver/templates/svn.vm
