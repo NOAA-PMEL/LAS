4a6ec8fcf1407ebcafbb31f8393106cc59aeeb07
malczyk <malczyk@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2008-10-30 19:18:55 +0000

    Adding browser sniff javascript
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@3512 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ WebContent/BrowserSniff.js
@@ -0,0 +1,75 @@
+//CS1.1
+
+var exclude=1;
+var agt=navigator.userAgent.toLowerCase();
+var win=0;var mac=0;var lin=1;
+if(agt.indexOf('win')!=-1){win=1;lin=0;}
+if(agt.indexOf('mac')!=-1){mac=1;lin=0;}
+var lnx=0;if(lin){lnx=1;}
+var ice=0;
+var ie=0;var ie4=0;var ie5=0;var ie6=0;var ie7=0;var ie8=0;var com=0;var dcm;
+var op5=0;var op6=0;var op7=0;
+var ns4=0;var ns6=0;var ns7=0;var mz7=0;var kde=0;var saf=0;
+if(typeof navigator.vendor!="undefined" && navigator.vendor=="KDE"){
+	var thisKDE=agt;
+	var splitKDE=thisKDE.split("konqueror/");
+	var aKDE=splitKDE[1].split("; ");
+	var KDEn=parseFloat(aKDE[0]);
+	if(KDEn>=2.2){
+		kde=1;
+		ns6=1;
+		exclude=0;
+		}
+	}
+else if(agt.indexOf('webtv')!=-1){exclude=1;}
+else if(typeof window.opera!="undefined"){
+	exclude=0;
+	if(/opera[\/ ][5]/.test(agt)){op5=1;}
+	if(/opera[\/ ][6]/.test(agt)){op6=1;}
+	if(/opera[\/ ][7-9]/.test(agt)){op7=1;}
+	}
+else if(typeof document.all!="undefined"&&!kde){
+	exclude=0;
+	ie=1;
+	if(typeof document.getElementById!="undefined"){
+		ie5=1;
+		if(agt.indexOf("msie 6")!=-1){
+			ie6=1;
+			dcm=document.compatMode;
+			if(dcm!="BackCompat")
+				{com=1;}
+		}
+		if(agt.indexOf("msie 7")!=-1){
+			ie7=1;
+			dcm=document.compatMode;
+			if(dcm!="BackCompat")
+				{com=1;}
+		}
+		if(agt.indexOf("msie 8")!=-1){
+			ie8=1;
+			dcm=document.compatMode;
+			if(dcm!="BackCompat")
+				{com=1;}
+		}
+
+	}
+	else{ie4=1;}
+}
+else if(typeof document.getElementById!="undefined"){
+	exclude=0;
+	if(agt.indexOf("netscape/6")!=-1||agt.indexOf("netscape6")!=-1){ns6=1;}
+	else if(agt.indexOf("netscape/7")!=-1||agt.indexOf("netscape7")!=-1){ns6=1;ns7=1;}
+	else if(agt.indexOf("gecko")!=-1){ns6=1;mz7=1;}
+	if(agt.indexOf("safari")!=-1 || (typeof document.childNodes!="undefined" && typeof document.all=="undefined" && typeof navigator.taintEnabled=="undefined")){mz7=0;ns6=1;saf=1;}
+	}
+else if((agt.indexOf('mozilla')!=-1)&&(parseInt(navigator.appVersion)>=4)){
+	exclude=0;
+	ns4=1;
+	if(typeof navigator.mimeTypes['*']=="undefined"){
+		exclude=1;
+		ns4=0;
+		}
+	}
+if(agt.indexOf('escape')!=-1){exclude=1;ns4=0;}
+if(typeof navigator.__ice_version!="undefined"){exclude=1;ie4=0;}
+
