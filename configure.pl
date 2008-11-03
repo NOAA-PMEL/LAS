3e15d88b0898e9c618b33ac16c8e57caef71a665
Roland Schweitzer <roland.schweitzer@noaa.gov>
2008-11-03 14:31:12 +0000

    Create build processes in the configure script and builld.xml to create a command line version of addXML.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@3533 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- configure.pl
+++ configure.pl
@@ -49,6 +49,7 @@ my @Scripts = qw(build.xml
                  xml/perl/testReq.pl
                  bin/initialize_check.sh
                  bin/las_ui_check.sh
+                 bin/addXML.sh
                  conf/example/sample_las.xml
                  conf/example/sample_ui.xml
                  conf/example/sample_insitu_las.xml
