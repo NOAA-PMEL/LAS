f28853a41e0ffbe0388ca4d1a5963f275dda3d94
Roland Schweitzer <roland.schweitzer@noaa.gov>
2009-06-02 15:07:24 +0000

    Rearranges the JDOM XML filters into a sub-package of las.jdom.  Implements an request input validation filter which check the names of every request parameter against a white list and checks the values of every parameter against a regular expression or enumerated list of acceptable values.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@4099 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ JavaSource/gov/noaa/pmel/tmap/las/filter/RequestInputFilter.java
@@ -0,0 +1,291 @@
+package gov.noaa.pmel.tmap.las.filter;
+
+import gov.noaa.pmel.tmap.las.jdom.JDOMUtils;
+import gov.noaa.pmel.tmap.las.jdom.LASUIRequest;
+import gov.noaa.pmel.tmap.las.product.server.LASAction;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class RequestInputFilter implements Filter {
+    private static Logger log = LogManager.getLogger(RequestInputFilter.class.getName());
+    // All LAS query parameters.
+    private final static String[] p = {
+    	"BBOX", 
+    	"cancel", 
+    	"catid", 
+    	"catitem", 
+    	"debug",
+    	"dsid",
+    	"email",
+    	"format",
+    	"JSESSIONID",
+		"log_level",
+		"opendap",
+		"opid",
+		"reinit", 
+		"REQUEST", 
+		"stream",
+		"stream_ID", 
+		"varid",
+		"view",
+		"xml"
+    };
+    private final static Set<String> LAS_PARAMETERS = new HashSet<String>(Arrays.asList(p));
+	
+    // Parameters that should be either "true" or "false"
+    private final static String[] bp = {
+    	"cancel",
+    	"debug",
+    	"stream"
+    };
+    private final static Set<String> LAS_BOOLEAN_PARAMETERS = new HashSet<String>(Arrays.asList(bp));
+    
+    // Every parameter that accepts an LAS ID
+    private static final String[] lp = {
+    	"catid", 
+    	"catitem", 
+    	"dsid",
+    	"JSESSIONID",
+		"opid",
+		"stream_ID", 
+		"varid",
+    };
+	
+    private final static Set<String> LAS_ID_PARAMETERS = new HashSet<String>(Arrays.asList(lp));
+    private final static Pattern ID_PATTERN = Pattern.compile("[A-Za-z0-9._-]+");
+    // Match 4 floating point numbers separated by a comma
+    private final static Pattern BBOX_PATTERN = Pattern.compile("[-+]?[0-9]*\\.?[0-9]+,[-+]?[0-9]*\\.?[0-9]+,[-+]?[0-9]*\\.?[0-9]+,[-+]?[0-9]*\\.?[0-9]+");
+    
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+		 if ( ! ( servletRequest instanceof HttpServletRequest ) )
+		    {
+		      log.error( "doFilter(): Not an HTTP request! How did this filter get here?" );
+		      filterChain.doFilter( servletRequest, servletResponse );
+		      return;
+		    }
+		    HttpServletRequest request = (HttpServletRequest) servletRequest;
+		    HttpServletResponse response = (HttpServletResponse) servletResponse;
+            if ( ! validateParameters(request) ) {
+            	LASAction.logerror(request, "Illegal request parameter.", "Request contains a parameter that is not allowed.");
+            	response.sendError(HttpServletResponse.SC_NOT_FOUND, "Request contains an illegal query parameter.");
+            	return;
+            }
+            if ( ! validBooleanValues(request) ) {
+            	LASAction.logerror(request, "Illegal request parameter value.", "Request contains a parameter value that is not allowed.");
+            	response.sendError(HttpServletResponse.SC_NOT_FOUND, "Request contains an illegal boolean query parameter value.");
+            	return;
+            }
+            if ( ! validIds(request) ) {
+            	LASAction.logerror(request, "Illegal request parameter value.", "Request contains a parameter value that is not allowed.");
+            	response.sendError(HttpServletResponse.SC_NOT_FOUND, "Request contains an illegal LAS ID query parameter value.");
+            	return;
+            }
+            if ( !validBBOX(request) ) {
+            	LASAction.logerror(request, "Illegal request parameter value.", "Request contains a parameter value that is not allowed.");
+            	response.sendError(HttpServletResponse.SC_NOT_FOUND, "Request contains an illegal BBOX query parameter value.");
+            	return;
+            }
+            if ( !validFormat(request) ) {
+            	LASAction.logerror(request, "Illegal request parameter value.", "Request contains a parameter value that is not allowed.");
+            	response.sendError(HttpServletResponse.SC_NOT_FOUND, "Request contains an illegal format query parameter value.");
+            	return;
+            }
+            if ( !validWMSRequest(request) ) {
+            	LASAction.logerror(request, "Illegal request parameter value.", "Request contains a parameter value that is not allowed.");
+            	response.sendError(HttpServletResponse.SC_NOT_FOUND, "Request contains an illegal WMS query parameter value.");
+            	return;
+            }
+            if ( !validView(request) ) {
+            	LASAction.logerror(request, "Illegal request parameter value.", "Request contains a parameter value that is not allowed.");
+            	response.sendError(HttpServletResponse.SC_NOT_FOUND, "Request contains an illegal view query parameter value.");
+            	return;
+            }
+            if ( !validReinit(request) ) {
+            	LASAction.logerror(request, "Illegal request parameter value.", "Request contains a parameter value that is not allowed.");
+            	response.sendError(HttpServletResponse.SC_NOT_FOUND, "Request contains an illegal reinit query parameter value.");
+            	return;
+            }
+            if ( !validLogLevel(request) ) {
+            	LASAction.logerror(request, "Illegal request parameter value.", "Request contains a parameter value that is not allowed.");
+            	response.sendError(HttpServletResponse.SC_NOT_FOUND, "Request contains an illegal log_level query parameter value.");
+            	return;
+            }
+            String requestXML = request.getParameter("xml");
+            if ( (requestXML != null && !requestXML.equals("")) ) {
+            	try {
+            		String temp = URLDecoder.decode(requestXML, "UTF-8");
+            		requestXML = temp;
+            	} catch (UnsupportedEncodingException e) {
+            		LASAction.logerror(request, "Error decoding the XML request query string.", e);
+            		response.sendError(HttpServletResponse.SC_NOT_FOUND, "Request contains an illegal xml query parameter value.");
+            		return;
+            	}
+
+            	// Create a lasRequest object.
+            	LASUIRequest lasRequest = new LASUIRequest();
+            	try {
+            		JDOMUtils.XML2JDOM(requestXML, lasRequest);
+            		// Set the lasRequest object in the HttpServletRequest so the product server does not have to rebuild it.
+            		request.setAttribute("las_request", lasRequest);
+            	} catch (Exception e) {
+            		LASAction.logerror(request, "Error parsing the request XML. ", e);
+            		response.sendError(HttpServletResponse.SC_NOT_FOUND, "Request contains an illegal xml query parameter value.");
+            		return;
+            	}
+            }
+		    filterChain.doFilter( servletRequest, servletResponse );
+		    return;
+	}
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		
+	}
+	private boolean validLogLevel(HttpServletRequest request) {
+		String value[] = request.getParameterValues("log_level");
+		if ( value == null || value.length == 0 ) {
+			return true;
+		}
+		if ( value.length > 1 ) return false;
+		
+		if ( !value[0].equals("debug") && 
+			 !value[0].equals("info") && 
+			 !value[0].equals("warn") && 
+			 !value[0].equals("error") && 
+			 !value[0].equals("fatal")
+			 ) return false;
+		return true;
+	}
+	private boolean validReinit(HttpServletRequest request ) {
+		String value[] = request.getParameterValues("reinit");
+		if ( value == null || value.length == 0 ) {
+			return true;
+		}
+		if ( value.length > 1 ) return false;
+		if ( !value[0].equals("wait") && !value[0].equals("force") ) return false;
+		return true;
+	}
+	private boolean validView(HttpServletRequest request) {
+		String value[] = request.getParameterValues("view");
+		if ( value == null || value.length == 0 ) {
+			return true;
+		}
+		if ( value.length > 1 ) return false;
+		
+		if ( !value[0].equals("x") && 
+			 !value[0].equals("y") && 
+			 !value[0].equals("z") && 
+			 !value[0].equals("t") && 
+			 !value[0].equals("xy") && 
+			 !value[0].equals("xz") && 
+			 !value[0].equals("xt") && 
+			 !value[0].equals("yx") && 
+			 !value[0].equals("yz") && 
+			 !value[0].equals("yt") && 
+			 !value[0].equals("tx") && 
+			 !value[0].equals("ty") && 
+			 !value[0].equals("tz") && 
+			 !value[0].equals("xyz") && 
+			 !value[0].equals("xyt") && 
+			 !value[0].equals("xzt") && 
+			 !value[0].equals("yzt") && 
+			 !value[0].equals("xyzt")
+			 ) return false;
+		return true;
+	}
+	private boolean validWMSRequest(HttpServletRequest request) {
+		String value[] = request.getParameterValues("REQUEST");
+		if ( value == null || value.length == 0 ) {
+			return true;
+		}
+		if ( value.length > 1 ) return false;
+		if ( !value[0].equals("GETMAP") && !value[0].equals("GETCAPABILITES") && !value[0].equals("GETFEATUREINFO") ) return false;
+		return true;
+	}
+	private boolean validFormat(HttpServletRequest request) {
+		String value[] = request.getParameterValues("format");
+		if ( value == null || value.length == 0 ) {
+			return true;
+		}
+		if ( value.length > 1 ) return false;
+		if ( !value[0].equals("json") || !value[0].equals("xml") ) return false;
+		return true;
+	}
+	private boolean validBBOX(HttpServletRequest request) {
+		String value[] = request.getParameterValues("BBOX");
+		if ( value == null || value.length == 0 ) {
+			return true;
+		}
+		if ( value.length > 1 ) return false; // only allow 1
+		Matcher m = BBOX_PATTERN.matcher(value[0]);
+		return m.matches();
+	}
+	// email address    "^[A-Za-z0-9._-]+@[[A-Za-z0-9.-]+$"
+	// LAS ID "^[A-Za-z0-9._-]"
+    private boolean validateParameters(HttpServletRequest request) {
+    	Set<String> parameters = request.getParameterMap().keySet();
+    	return LAS_PARAMETERS.containsAll(parameters);
+    }
+    private boolean validBooleanValues(HttpServletRequest request) {
+    	boolean valid = true;
+    	for (Iterator keyIt = LAS_BOOLEAN_PARAMETERS.iterator(); keyIt.hasNext();) {
+			String key = (String) keyIt.next();
+			String[] value = request.getParameterValues(key);
+			valid = valid && validateBooleanValue(value);			
+		}
+    	return valid;
+    }
+    private boolean validateBooleanValue(String[] value) {
+
+		if ( value == null || value.length == 0 ) return true;
+		
+		if ( value.length > 1 ) return false;
+		
+		if ( !value[0].equals("true") && !value[0].equals("false") ) return false;
+		
+		return true;
+    }
+
+	private boolean validIds(HttpServletRequest request) {
+		boolean valid = true;
+    	for (Iterator keyIt = LAS_ID_PARAMETERS.iterator(); keyIt.hasNext();) {
+			String key = (String) keyIt.next();
+			String[] value = request.getParameterValues(key);
+			valid = valid && validateIdValue(value);			
+		}
+    	return valid;
+	}
+
+	private boolean validateIdValue(String[] value) {
+		if ( value == null || value.length == 0 ) return true;
+		// More than one is allowed so don't test the length
+		Matcher m = ID_PATTERN.matcher(value[0]);
+		return m.matches();
+	}
+}
--- JavaSource/gov/noaa/pmel/tmap/las/ui/Util.java
+++ JavaSource/gov/noaa/pmel/tmap/las/ui/Util.java
@@ -7,6 +7,8 @@ import gov.noaa.pmel.tmap.las.util.Option;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jdom.Attribute;
 import org.jdom.Element;
@@ -172,5 +174,100 @@ public class Util {
 		}
 		return json;
 	}
+
+	/*  Thanks Ethan and John... */
+	
+	/*
+	 * Copyright 1998-2009 University Corporation for Atmospheric Research/Unidata
+	 *
+	 * Portions of this software were developed by the Unidata Program at the
+	 * University Corporation for Atmospheric Research.
+	 *
+	 * Access and use of this software shall impose the following obligations
+	 * and understandings on the user. The user is granted the right, without
+	 * any fee or cost, to use, copy, modify, alter, enhance and distribute
+	 * this software, and any derivative works thereof, and its supporting
+	 * documentation for any purpose whatsoever, provided that this entire
+	 * notice appears in all copies of the software, derivative works and
+	 * supporting documentation.  Further, UCAR requests that the user credit
+	 * UCAR/Unidata in any publications that result from the use of this
+	 * software or in any product that includes this software. The names UCAR
+	 * and/or Unidata, however, may not be used in any advertising or publicity
+	 * to endorse or promote any products or commercial entity unless specific
+	 * written permission is obtained from UCAR/Unidata. The user also
+	 * understands that UCAR/Unidata is not obligated to provide the user with
+	 * any support, consulting, training or assistance of any kind with regard
+	 * to the use, operation and performance of this software nor to provide
+	 * the user with any updates, revisions, new versions or "bug fixes."
+	 *
+	 * THIS SOFTWARE IS PROVIDED BY UCAR/UNIDATA "AS IS" AND ANY EXPRESS OR
+	 * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+	 * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	 * DISCLAIMED. IN NO EVENT SHALL UCAR/UNIDATA BE LIABLE FOR ANY SPECIAL,
+	 * INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING
+	 * FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+	 * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+	 * WITH THE ACCESS, USE OR PERFORMANCE OF THIS SOFTWARE.
+	 */
+
+	 /**
+	   * Return true if the given String is a valid path.
+	   *
+	   * <p> 
+	   * A String is considered a valid path if:
+	   * <ul>
+	   *   <li> when passed to validSingleLineString(String) true is returned, and
+	   *   <li> it does not contain any parent path segments ("../").</li>
+	   *   </li>
+	   * </ul>
+	   *
+	   * @param path the String to validate
+	   * @return true if the given String is a valid path.
+	   * @see #validSingleLineString(String)
+	   */  
+	  @SuppressWarnings({"SimplifiableIfStatement"})
+	  public static boolean validPath( String path )
+	  {
+	    if ( path == null )
+	      return false;
+	    // Don't allow ".." directories in path.
+	    if ( path.indexOf( "/../" ) != -1 || path.equals( ".." )
+	         || path.startsWith( "../" ) || path.endsWith( "/.." ) )
+	      return false;
+
+	    return validSingleLineString( path );
+	  }
+
+	 /**
+	   * Return true if the given String is a valid single-line String.
+	   *
+	   * <p>A string will be considered a valid single-line string if it does not
+	   * contain any characters from these Unicode general categories:
+	   *
+	   * <ul>
+	   *   <li>Cc - Other, Control</li>
+	   *   <li>Cf - Other, Format</li>
+	   *   <li>Cs - Other, Surrogate</li>
+	   *   <li>Co - Other, Private Use</li>
+	   *   <li>Cn - Other, Not Assigned</li>
+	   *   <li>Zl - Separator, Line</li>
+	   *   <li>Zp - Separator, Paragraph</li>
+	   * </ul>
+	   *
+	   * <p>Or, in other words, allow: Letters, Numbers, Marks, Punctuation,
+	   * Symbols, and Space separators.
+	   *
+	   * @param singleLineString the String to validate
+	   * @return true if the given String is a valid single-line String.
+	   */
+	  public static boolean validSingleLineString( String singleLineString )
+	  {
+	    if ( singleLineString == null ) return false;
+	    Matcher m = INVALID_CHARACTERS_FOR_SINGLE_LINE_STRING_PATTERN.matcher( singleLineString );
+	    return ! m.find();
+	  }
+	  private final static Pattern INVALID_CHARACTERS_FOR_SINGLE_LINE_STRING_PATTERN
+	          = Pattern.compile( "[\\p{Zl}\\p{Zp}\\p{C}]");
+
 }
 
