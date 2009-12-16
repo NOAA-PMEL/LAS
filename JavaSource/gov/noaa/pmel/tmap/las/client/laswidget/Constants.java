5cf1e85613179b5f31de30b1ad9dd185b9afbc76
Roland Schweitzer <roland.schweitzer@noaa.gov>
2009-12-16 22:15:52 +0000

    Add explicit buttons for all map actions.  Add RCP to get the default data set and variable and use the information in the TestUI and add the featureAdded call back to the region widget listener.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@4590 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ JavaSource/gov/noaa/pmel/tmap/las/client/laswidget/Constants.java
@@ -0,0 +1,13 @@
+package gov.noaa.pmel.tmap.las.client.laswidget;
+
+public class Constants {
+
+	public static final String DEFAULT_DSID = "default_dsid";
+	public static final String DEFAULT_VARID = "default_varid";
+	public static final String DEFAULT_OP = "default_operation";
+	public static final String DEFAULT_OPTION = "default_option";
+	public static final String DEFAULT_VIEW = "default_view";
+	public static final String DEFAULT_TIME = "default_time";
+	public static final String DEFAULT_Z = "default_z";
+	
+}
