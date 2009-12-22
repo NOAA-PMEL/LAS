0f51bb2f08e98dbff0de825311b707eaf80b5277
rhs <rhs@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2009-12-22 22:41:32 +0000

    Use new RPC call to get variable directly to initialize vizGal.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@4636 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/las/client/RPCServiceAsync.java
+++ JavaSource/gov/noaa/pmel/tmap/las/client/RPCServiceAsync.java
@@ -4,6 +4,7 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface RPCServiceAsync {
 	public void getCategories(String id, AsyncCallback categoryCallback);
+	public void getVariable(String dsid, String varid, AsyncCallback getVariableCallback);
     public void getGrid(String dsid, String varid, AsyncCallback gridCallback);
     public void getOperations(String view, String dsid, String varid, AsyncCallback opCallback);
 	public void getOptions(String opid, AsyncCallback optionsCallback);
