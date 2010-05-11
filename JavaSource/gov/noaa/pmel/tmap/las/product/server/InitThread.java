898ef0f88e449a26544fcb512b5985aa6bcb40ba
Roland Schweitzer <roland.schweitzer@noaa.gov>
2010-05-11 20:19:36 +0000

    Log the start and end of the initialization.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@5169 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/las/product/server/InitThread.java
+++ JavaSource/gov/noaa/pmel/tmap/las/product/server/InitThread.java
@@ -15,10 +15,12 @@ public class InitThread extends Thread {
 	public void run() {
 		LASConfigPlugIn plugin = new LASConfigPlugIn();
 		try {
+                        log.info("Starting server initialization.");
 			plugin.reinit(context);
 		} catch (ServletException e) {
 			log.error("Error in LAS init thread.");
 		}
 		context.removeAttribute(LASConfigPlugIn.LAS_LAZY_START_KEY);
+                log.info("Initialization complete.  Server ready for requests.");
 	}
 }
