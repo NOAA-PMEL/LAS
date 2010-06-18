7adac53fec108cdc626e2fd280a0e40ff34dcee8
Roland Schweitzer <roland.schweitzer@noaa.gov>
2010-06-18 00:19:13 +0000

    Add the Util class.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@5414 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ JavaSource/gov/noaa/pmel/tmap/addxml/Util.java
@@ -0,0 +1,18 @@
+package gov.noaa.pmel.tmap.addxml;
+
+public class Util {
+	public static final char[][] vectorPatterns = new char[][]{{'x', 'X', 'u', 'U'},{'y', 'Y', 'v', 'V'},{'z', 'Z', 'w', 'W'}};
+	public static final String[] vectorRanges = new String[]{"[xyz]", "[XYZ]", "[uvw]", "[UVW]"};
+	public static int countOccurrences(String haystack, char needle)
+	{
+	    int count = 0;
+	    for (int i=0; i < haystack.length(); i++)
+	    {
+	        if (haystack.charAt(i) == needle)
+	        {
+	             count++;
+	        }
+	    }
+	    return count;
+	}
+}
