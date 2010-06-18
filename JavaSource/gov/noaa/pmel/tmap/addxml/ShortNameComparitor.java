dbfdd9ae698b857489e04a6981316092d1d56aeb
rhs <rhs@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2010-06-18 00:21:41 +0000

    Add the ShortNameComparator to sort variable by their netCDF name.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@5415 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ JavaSource/gov/noaa/pmel/tmap/addxml/ShortNameComparitor.java
@@ -0,0 +1,12 @@
+package gov.noaa.pmel.tmap.addxml;
+
+import java.util.Comparator;
+
+public class ShortNameComparitor implements Comparator<VariableBean> {
+
+	@Override
+	public int compare(VariableBean o1, VariableBean o2) {
+		return o1.getShortName().compareTo(o2.getShortName());
+	}
+
+}
