2616f2d5014b6c4c1b1ce5f6ddb981feded3d55a
Roland Schweitzer <roland.schweitzer@noaa.gov>
2010-07-14 14:40:16 +0000

    Add the license comment, clean up the imports and finish the map widget distribution ant targets.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@5566 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/las/client/openlayers/DrawSingleFeatureImpl.java
+++ JavaSource/gov/noaa/pmel/tmap/las/client/openlayers/DrawSingleFeatureImpl.java
@@ -1,4 +1,20 @@
-package gov.noaa.pmel.tmap.las.client.openlayers;
+
+/**
+ * This software was developed by Roland Schweitzer of Weathertop Consulting, LLC 
+ * (http://www.weathertopconsulting.com/) as part of work performed for
+ * NOAA Contracts AB113R-04-RP-0068 and AB113R-09-CN-0182.  
+ * 
+ * The NOAA licensing terms are explained below.
+ * 
+ * 
+ * This software is provided by NOAA for full, free and open release.  It is
+ * understood by the recipient/user that NOAA assumes no liability for any
+ * errors contained in the code.  Although this software is released without
+ * conditions or restrictions in its use, it is expected that appropriate
+ * credit be given to its author and to the National Oceanic and Atmospheric
+ * Administration should the software be included by the recipient as an
+ * element in other product development. 
+ */package gov.noaa.pmel.tmap.las.client.openlayers;
 
 import gov.noaa.pmel.tmap.las.client.openlayers.DrawSingleFeature.FeatureAddedListener;
 
