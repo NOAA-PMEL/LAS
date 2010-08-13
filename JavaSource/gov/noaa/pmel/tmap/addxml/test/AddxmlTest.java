6f05c33ff0dee188fae958d6edb8570a8ea9b1f2
rhs <rhs@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2010-08-13 19:48:47 +0000

    Add check to prevent variables that start with a vector heuristic, but don't have at least one partner variable from getting made into composites.  Code and JUnit test.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@5703 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/addxml/test/AddxmlTest.java
+++ JavaSource/gov/noaa/pmel/tmap/addxml/test/AddxmlTest.java
@@ -70,7 +70,37 @@ public class AddxmlTest {
 		}
 		
 	}
-    
+    @Test
+    public final void testGFDL() {
+    	// Test to make sure variables with names that start with u or v but aren't vectors don't get included.  There should only be one composite.
+    	String url = DODSNetcdfFile.canonicalURL("http://data1.gfdl.noaa.gov:8380/thredds3/dodsC/dc_CM2.1_R1_Cntr-ITFblock_monthly_ocean_tripolar_01010101-02001231");
+		String[] name = new String[]{"Vector of Grid_eastward Sea Water Velocity and Grid_northward Sea Water Velocity and Upward Sea Water Velocity"};
+		String[] units = new String[]{"m s-1"};
+		NetcdfDataset ncds;
+		try {
+			ncds = ucar.nc2.dataset.NetcdfDataset.openDataset(url);
+			// Test from COADS, which tests the new vectors capability
+			Document leetmaa = addXML.createXMLfromNetcdfDataset(ncds , url);
+			Iterator compIt = leetmaa.getRootElement().getDescendants(new ElementFilter("composite"));
+			assertTrue(compIt.hasNext());
+			int index = 0;
+			while ( compIt.hasNext() ) {
+				Element composite = (Element) compIt.next();
+				List children = composite.getChildren();				
+				assertTrue(children.size() == 1);
+				Element variable = (Element) children.get(0);
+				assertTrue(variable != null);
+				String vname = variable.getAttributeValue("name");
+				String vunits = variable.getAttributeValue("units");
+				assertTrue(vname.equals(name[index]));
+				assertTrue(vunits.equals(units[index]));
+				index++;
+			}
+			assertTrue(index == 1);
+		} catch (IOException e) {
+			fail("Unable to connect to OPeNDAP server.");
+		}
+    }
 	@Test
 	public final void testLeetmaa() {
 		String url = DODSNetcdfFile.canonicalURL("http://iridl.ldeo.columbia.edu/SOURCES/.NOAA/.NCEP/.EMC/.CMB/.Pacific/.monthly/dods");
