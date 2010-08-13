a1fbf4fb764943a3de0a0f0e051db10bebb10d6a
malczyk <malczyk@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2010-08-13 00:31:02 +0000

    Chcanign operation order and removing some vestigial javascript
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@5702 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 LAS_HOME=${PWD/#$HOME\//}
 CATALINA_HOME=../tomcat
-cp -Ruv WebContent/* ../tomcat/webapps/las/
-cp -Ruv JavaSource/resources/* $CATALINA_HOME/webapps/las/WEB-INF/classes/resources/
+cp -Ruv WebContent/* ../tomcat/webapps/las_dev/
+cp -Ruv JavaSource/resources/* $CATALINA_HOME/webapps/las_dev/WEB-INF/classes/resources/
