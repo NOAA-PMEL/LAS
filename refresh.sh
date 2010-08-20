7cb7be6fb1337b7bc0084787d32db774c03ccb02
Jeremy Malczyk <jeremy.malczyk@noaa.gov>
2010-08-20 23:40:06 +0000

    Updating compiled javascript
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@5755 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 LAS_HOME=${PWD/#$HOME\//}
 CATALINA_HOME=../tomcat
-cp -Ruv WebContent/* ../tomcat/webapps/las_dev/
-cp -Ruv JavaSource/resources/* $CATALINA_HOME/webapps/las_dev/WEB-INF/classes/resources/
+cp -Ruv WebContent/* ../tomcat/webapps/$LAS_HOME/
+cp -Ruv JavaSource/resources/* $CATALINA_HOME/webapps/$LAS_HOME/WEB-INF/classes/resources/
