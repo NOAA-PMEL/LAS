e7391bd075d21944826d696e9d13d55818a2a97e
rhs <rhs@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2010-08-20 02:34:04 +0000

    Automatically detect high frequency time data.  Break tests into separate classes with a test suite so it's easier to test individual failures.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@5747 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ JavaSource/gov/noaa/pmel/tmap/addxml/test/TestLeetmaa.java
@@ -0,0 +1,66 @@
+package gov.noaa.pmel.tmap.addxml.test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import gov.noaa.pmel.tmap.addxml.addXML;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.filter.ElementFilter;
+import org.junit.Test;
+
+import ucar.nc2.dataset.NetcdfDataset;
+import ucar.nc2.dods.DODSNetcdfFile;
+
+
+public class TestLeetmaa extends TestCase {
+	@Test
+	public final void testLeetmaa() {
+		String url = DODSNetcdfFile.canonicalURL("http://iridl.ldeo.columbia.edu/SOURCES/.NOAA/.NCEP/.EMC/.CMB/.Pacific/.monthly/dods");
+		String[] name = new String[]{"Vector of zonal wind stress and meridional wind stress", "Vector of zonal velocity and meridional velocity"};
+		String[] units = new String[]{"unitless", "cm/s"};
+		NetcdfDataset ncds;
+		try {
+			addXML addxml = new addXML();
+			HashMap<String, String> options= new HashMap<String, String>();
+			addxml.setOptions(options);
+			ncds = ucar.nc2.dataset.NetcdfDataset.openDataset(url);
+			// Test from COADS, which tests the new vectors capability
+			Document leetmaa = addxml.createXMLfromNetcdfDataset(ncds , url);
+			Iterator compIt = leetmaa.getRootElement().getDescendants(new ElementFilter("composite"));
+			assertTrue(compIt.hasNext());
+			int index = 0;
+			while ( compIt.hasNext() ) {
+				Element composite = (Element) compIt.next();
+				List children = composite.getChildren();				
+				assertTrue(children.size() == 1);
+				Element variable = (Element) children.get(0);
+				assertTrue(variable != null);
+				String vname = variable.getAttributeValue("name");
+				String vunits = variable.getAttributeValue("units");
+				assertTrue(vname.equals(name[index]));
+				assertTrue(vunits.equals(units[index]));
+				index++;
+			}
+			Iterator arIt = leetmaa.getDescendants(new ElementFilter("arange"));
+			assertTrue(arIt.hasNext());
+			while ( arIt.hasNext() ) {
+				Element arange = (Element) arIt.next();
+				String start = arange.getAttributeValue("start");
+				if ( start.length() > 6 ) {
+					// It's the time...
+					assertTrue(start.equals("1980-01-01"));
+				}
+			}
+		} catch (IOException e) {
+			fail("Unable to connect to OPeNDAP server.");
+		}
+	}
+}
