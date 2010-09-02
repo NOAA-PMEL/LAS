e68f34715b2d852371f88f28f380b7a22727f106
Roland Schweitzer <roland.schweitzer@noaa.gov>
2010-09-02 15:23:32 +0000

    Reduce the restrictions since we're not comparing.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@5844 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/las/client/TestUI.java
+++ JavaSource/gov/noaa/pmel/tmap/las/client/TestUI.java
@@ -56,7 +56,7 @@ public class TestUI implements EntryPoint {
 	HTML output;
 	VizGalPanel panel;
 	Grid layout = new Grid(2, 1);
-	
+
 	String dsid;
 	String vid;
 	String op;
@@ -81,18 +81,18 @@ public class TestUI implements EntryPoint {
 	DisclosurePanel settingsHeader = new DisclosurePanel("Settings");
 	AxesWidgetGroup axesWidget = new AxesWidgetGroup("Plot Axes", "Other Axes", "vertical", settingsWidth, "Axes");
 	OperationsWidget operationsWidget = new OperationsWidget("operations");
-	
+
 	// Top nav
 	FlexTable buttonControls = new FlexTable();
 	DatasetButton datasetButton;
 	OptionsButton optionsButton;
 	OperationsMenu operationsMenu;
-	
+
 	DockPanel dockPanel = new DockPanel();
 	PopupPanel initializing = new PopupPanel();
-	
+
 	OperationSerializable[] ops;
-	
+
 	String xlo;
 	String xhi;
 	String ylo;
@@ -101,33 +101,33 @@ public class TestUI implements EntryPoint {
 	String zhi;
 	String tlo;
 	String thi;
-	
+
 	/*
 	 * Keep track of the new variable and change state to be able apply data set changes only after apply button is pressed.
 	 */
 	VariableSerializable nvar;
 	boolean changeDataset = false;
-    // Sometimes  you need to keep the current map selection values.
-    double[] cs = null;
-    
-    DisclosurePanel hideControls = new DisclosurePanel("");
-    
-    boolean operationsPanelIsOpen = true;
-    boolean panelHeaderHidden = false;
-    
-    int pwidth = 0;
-    /*
+	// Sometimes  you need to keep the current map selection values.
+	double[] cs = null;
+
+	DisclosurePanel hideControls = new DisclosurePanel("");
+
+	boolean operationsPanelIsOpen = true;
+	boolean panelHeaderHidden = false;
+
+	int pwidth = 0;
+	/*
 	 * Image size control.
 	 */
 	ListBox imageSize;
 	Label imageSizeLabel = new Label("Image zoom: ");
-	
+
 	public void onModuleLoad() {
-		
+
 		String spinImageURL = URLUtil.getImageURL()+"/mozilla_blu.gif";
 		output = new HTML("<img src=\""+spinImageURL+"\" alt=\"Spinner\"/> Initializing...");
-	    initializing.add(output);
-	    initializing.show();
+		initializing.add(output);
+		initializing.show();
 		Map<String, List<String>> parameters = Window.Location.getParameterMap();
 		dsid = Util.getParameterString("dsid");
 		vid = Util.getParameterString("vid");
@@ -142,7 +142,7 @@ public class TestUI implements EntryPoint {
 		zhi = Util.getParameterString("zhi");
 		tlo = Util.getParameterString("tlo");
 		thi = Util.getParameterString("thi");
-		
+
 		if ( dsid != null && vid != null && op != null && view != null && option != null) {
 			Util.getRPCService().getCategories(dsid, initPanelFromParametersCallback);
 		} else {
@@ -163,7 +163,7 @@ public class TestUI implements EntryPoint {
 				operationsWidget.setOpen(false);
 				axesWidget.setOpen(false);
 			}
-			
+
 		});
 		settingsHeader.addOpenHandler(new OpenHandler<DisclosurePanel>() {
 
@@ -172,18 +172,18 @@ public class TestUI implements EntryPoint {
 				operationsWidget.setOpen(true);
 				axesWidget.setOpen(true);
 			}
-			
+
 		});
 		axesWidget.addApplyHandler(settingsButtonApplyHandler);
 		settingsControls = new FlexTable();
 		settingsControls.setWidget(0, 0, settingsHeader);
-		
+
 		settingsControls.setWidget(1, 0, axesWidget);
 		settingsControls.setWidget(2, 0, operationsWidget);
 		operationsWidget.addClickHandler(operationsClickHandler);
 		operationsMenu = new OperationsMenu();
 		operationsMenu.addClickHandler(externalOperationClick);
-		
+
 		datasetButton = new DatasetButton();
 		datasetButton.ensureDebugId("datasetButton");
 		datasetButton.addTreeListener(datasetTreeListener);
@@ -225,9 +225,9 @@ public class TestUI implements EntryPoint {
 			public void onChange(ChangeEvent arg0) {
 				String value = imageSize.getValue(imageSize.getSelectedIndex());
 				if ( !value.equals("auto") ) {
-					
-						panel.setImageSize(Integer.valueOf(value).intValue());
-					
+
+					panel.setImageSize(Integer.valueOf(value).intValue());
+
 				} else {
 					if ( pwidth == 0 ) {
 						int win = Window.getClientWidth();
@@ -244,8 +244,8 @@ public class TestUI implements EntryPoint {
 				}
 			}		
 		});
-		
-		
+
+
 		buttonControls.setWidget(0, 0, hideControls);
 		buttonControls.setWidget(0, 1, datasetButton);
 		buttonControls.setWidget(0, 2, optionsButton);
@@ -254,7 +254,7 @@ public class TestUI implements EntryPoint {
 		buttonControls.setWidget(0, 5, imageSize);
 		dockPanel.add(buttonControls, DockPanel.NORTH);
 		dockPanel.add(settingsControls, DockPanel.WEST);
-		
+
 		RootPanel.get("main").add(dockPanel);	
 		Window.addWindowResizeListener(windowResizeListener);
 	}
@@ -345,7 +345,7 @@ public class TestUI implements EntryPoint {
 		}
 	};
 	private void initPanel() {
-		
+
 		initializing.hide();
 		if ( view == null ) {
 			view = "xy";
@@ -361,10 +361,10 @@ public class TestUI implements EntryPoint {
 		axesWidget.getRefMap().setTool(view);
 		axesWidget.getRefMap().setDataExtent(grid_south, grid_north, grid_west, grid_east, delta);
 		operationsWidget.setOperations(var.getIntervals(), var.getDSID(), var.getID(), op, view);
-        operationsMenu.setMenus(ops, view);
+		operationsMenu.setMenus(ops, view);
 		// Examine the variable axes and determine which are orthogonal to the view. 
-		
-		
+
+
 		if ( var.getGrid().getXAxis() != null && !view.contains("x") ) {
 			ortho.add("x");
 		}
@@ -378,74 +378,69 @@ public class TestUI implements EntryPoint {
 			ortho.add("t");
 		}
 
-		if ( ortho.size() == 0 ) {
-			Window.alert("There are no axes orthogonal to the view on which the data can be compared.");
-		} else if ( ortho.size() > 2 ) { 
-			Window.alert("There are "+ortho.size()+" orthogonal axes.  The SlideSorterOld only allows 2.");
-		} else {
-			
-			axesWidget.init(var.getGrid());
-			axesWidget.setFixedAxis(view, ortho, null);		
-			
-			if ( panel != null ) {
-				dockPanel.remove(panel);
-			}
-			panel = new VizGalPanel("LAS", true, op, option, view, true);
-			panel.setVariable(var);
-			panel.init(false, ops);
 
-			panel.addApplyHandler(panelApply);
-			
-			// Move the current state of the axes to the panel
-			if ( tlo != null && !tlo.equals("") ) {
-				panel.setAxisRangeValues("t", tlo, thi);
-				axesWidget.getTAxis().setLo(tlo);
-				axesWidget.getTAxis().setHi(thi);
-			} else {
-				if ( var.getGrid().hasT() ) {
-					panel.setAxisRangeValues("t", axesWidget.getTAxis().getFerretDateLo(), axesWidget.getTAxis().getFerretDateHi());
-				}
-			}
-			if ( zlo != null && !zlo.equals("") ) {
-				panel.setAxisRangeValues("z", zlo, zhi);
-				axesWidget.getZAxis().setLo(zlo);
-				axesWidget.getZAxis().setHi(zhi);
-			} else {
-				if ( var.getGrid().hasZ() ) {
-					panel.setAxisRangeValues("z", axesWidget.getZAxis().getHi(), axesWidget.getZAxis().getLo());
-				}
-			}
+		axesWidget.init(var.getGrid());
+		axesWidget.setFixedAxis(view, ortho, null);		
 
-
-			// If these limits are not the same as the dataBounds, then set them.
-			if ( xlo != null && !xlo.equals("") && xhi != null && !xhi.equals("") && 
-				 ylo != null && !ylo.equals("") && yhi != null && !yhi.equals("") ) {
-				axesWidget.getRefMap().setCurrentSelection(Double.valueOf(ylo), Double.valueOf(yhi), Double.valueOf(xlo), Double.valueOf(xhi));
-				panel.setMapTool(view);
-				panel.setLatLon(ylo, yhi, xlo, xhi);
-			} else {
-				double tmp_xlo = axesWidget.getRefMap().getXlo();
-				double tmp_xhi = axesWidget.getRefMap().getXhi();
-				double tmp_ylo = axesWidget.getRefMap().getYlo();
-				double tmp_yhi = axesWidget.getRefMap().getYhi();
-			    panel.setMapTool(view);
-				panel.setLatLon(String.valueOf(tmp_ylo), String.valueOf(tmp_yhi), String.valueOf(tmp_xlo), String.valueOf(tmp_xhi));
-			}
-					
-			panel.refreshPlot(null, false, false);
-			int win = Window.getClientWidth();
-			if ( panelHeaderHidden ) {
-				pwidth = (win - rightPad);
-			} else {
-				pwidth = (win-(rightPad+controlsWidth));
-			}
-			if ( pwidth <= 0 ) {
-				pwidth = 400;
-			}
-			panel.setPanelWidth(pwidth);
-			dockPanel.add(panel, DockPanel.CENTER);
-			
+		if ( panel != null ) {
+			dockPanel.remove(panel);
 		}
+		panel = new VizGalPanel("LAS", true, op, option, view, true);
+		panel.setVariable(var);
+		panel.init(false, ops);
+
+		panel.addApplyHandler(panelApply);
+
+		// Move the current state of the axes to the panel
+		if ( tlo != null && !tlo.equals("") ) {
+			panel.setAxisRangeValues("t", tlo, thi);
+			axesWidget.getTAxis().setLo(tlo);
+			axesWidget.getTAxis().setHi(thi);
+		} else {
+			if ( var.getGrid().hasT() ) {
+				panel.setAxisRangeValues("t", axesWidget.getTAxis().getFerretDateLo(), axesWidget.getTAxis().getFerretDateHi());
+			}
+		}
+		if ( zlo != null && !zlo.equals("") ) {
+			panel.setAxisRangeValues("z", zlo, zhi);
+			axesWidget.getZAxis().setLo(zlo);
+			axesWidget.getZAxis().setHi(zhi);
+		} else {
+			if ( var.getGrid().hasZ() ) {
+				panel.setAxisRangeValues("z", axesWidget.getZAxis().getHi(), axesWidget.getZAxis().getLo());
+			}
+		}
+
+
+		// If these limits are not the same as the dataBounds, then set them.
+		if ( xlo != null && !xlo.equals("") && xhi != null && !xhi.equals("") && 
+				ylo != null && !ylo.equals("") && yhi != null && !yhi.equals("") ) {
+			axesWidget.getRefMap().setCurrentSelection(Double.valueOf(ylo), Double.valueOf(yhi), Double.valueOf(xlo), Double.valueOf(xhi));
+			panel.setMapTool(view);
+			panel.setLatLon(ylo, yhi, xlo, xhi);
+		} else {
+			double tmp_xlo = axesWidget.getRefMap().getXlo();
+			double tmp_xhi = axesWidget.getRefMap().getXhi();
+			double tmp_ylo = axesWidget.getRefMap().getYlo();
+			double tmp_yhi = axesWidget.getRefMap().getYhi();
+			panel.setMapTool(view);
+			panel.setLatLon(String.valueOf(tmp_ylo), String.valueOf(tmp_yhi), String.valueOf(tmp_xlo), String.valueOf(tmp_xhi));
+		}
+
+		panel.refreshPlot(null, false, false);
+		int win = Window.getClientWidth();
+		if ( panelHeaderHidden ) {
+			pwidth = (win - rightPad);
+		} else {
+			pwidth = (win-(rightPad+controlsWidth));
+		}
+		if ( pwidth <= 0 ) {
+			pwidth = 400;
+		}
+		panel.setPanelWidth(pwidth);
+		dockPanel.add(panel, DockPanel.CENTER);
+
+
 	}
 	public void applyChange() {
 		if ( changeDataset ) {
@@ -468,13 +463,13 @@ public class TestUI implements EntryPoint {
 			if ( var.getGrid().hasZ() ) {
 				panel.setAxisRangeValues("z", axesWidget.getZAxis().getHi(), axesWidget.getZAxis().getLo());
 			}
-			
+
 			double tmp_xlo = axesWidget.getRefMap().getXlo();
 			double tmp_xhi = axesWidget.getRefMap().getXhi();
 
 			double tmp_ylo = axesWidget.getRefMap().getYlo();
 			double tmp_yhi = axesWidget.getRefMap().getYhi();
-			
+
 			panel.setLatLon(String.valueOf(tmp_ylo), String.valueOf(tmp_yhi), String.valueOf(tmp_xlo), String.valueOf(tmp_xhi));
 			// TODO GEt an options button for kripes sake Map<String, String> temp_state = new HashMap<String, String>(optionsButton.getState());
 			panel.refreshPlot(null, false, true);
@@ -485,7 +480,7 @@ public class TestUI implements EntryPoint {
 	 */
 	public void changeDataset() {
 		var = nvar;
-		
+
 		changeDataset = false;
 
 		// Since we are changing data sets, go to the default plot and view.
@@ -504,7 +499,7 @@ public class TestUI implements EntryPoint {
 		// Get all the config info.  View is null to get all operations.
 		//TODO a call back for getting the grid of the new variable...
 		Util.getRPCService().getConfig(null, var.getDSID(), var.getID(), null);
-		
+
 	}
 	ClickHandler settingsButtonApplyHandler = new ClickHandler() {
 
@@ -516,34 +511,34 @@ public class TestUI implements EntryPoint {
 	ClickHandler panelApply = new ClickHandler() {
 		@Override
 		public void onClick(ClickEvent arg0) {
-//			panel.setLatLon(String.valueOf(settingsControls.getRefMap().getYlo()), String.valueOf(settingsControls.getRefMap().getYhi()), String.valueOf(settingsControls.getRefMap().getXlo()), String.valueOf(settingsControls.getRefMap().getXhi()));
+			//			panel.setLatLon(String.valueOf(settingsControls.getRefMap().getYlo()), String.valueOf(settingsControls.getRefMap().getYhi()), String.valueOf(settingsControls.getRefMap().getXlo()), String.valueOf(settingsControls.getRefMap().getXhi()));
 			panel.refreshPlot(optionsButton.getState(), false, true);
 		}		
 	};
 	AsyncCallback<ConfigSerializable> getGridCallback = new AsyncCallback<ConfigSerializable>() {
 		public void onSuccess(ConfigSerializable config) {
-            
+
 			GridSerializable grid = config.getGrid();
 			ops = config.getOperations();
 			var.setGrid(grid);
 			initPanel();
-			
+
 		}
 
 		@Override
 		public void onFailure(Throwable caught) {
 			initializing.hide();
 			Window.alert("Could not fetch grid.  "+caught.getLocalizedMessage());
-			
+
 		}
-		
+
 	};
 	// In vizGal we monitor the window width since there are two across.  In this case we want the height to fit.
 	public WindowResizeListener windowResizeListener = new WindowResizeListener() {
 		public void onWindowResized(int width, int height) {
 			int pheight = (height-topPad);
 			if (panel != null ) {
-	            panel.setPanelHeight(pheight);
+				panel.setPanelHeight(pheight);
 			}
 		}
 	};
@@ -597,29 +592,29 @@ public class TestUI implements EntryPoint {
 			String features = "toolbar=1,location=1,directories=1,status=1,menubar=1,scrollbars=1,resizable=1"; 
 			Window.open(Util.getProductServer()+"?xml="+URL.encode(lasRequest.getXMLText()), op.getName(), features);
 		}
-		
+
 	};
 	ClickHandler datasetOpenHandler = new ClickHandler() {
 
 		@Override
 		public void onClick(ClickEvent arg0) {
-			
+
 			axesWidget.closePanels();
 			operationsPanelIsOpen = operationsWidget.isOpen();
 			operationsWidget.setOpen(false);
-			
+
 		}
-		
+
 	};
 	ClickHandler datasetCloseHandler = new ClickHandler() {
 
 		@Override
 		public void onClick(ClickEvent arg0) {
-			
+
 			axesWidget.restorePanels();
 			operationsWidget.setOpen(operationsPanelIsOpen);
 		}
-		
-		
+
+
 	};
 }
