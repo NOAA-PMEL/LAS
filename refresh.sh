a5defd3d010555c1bddefa9f057b34eb67c62d68
malczyk <malczyk@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2010-09-14 18:30:59 +0000

    Adding mapResize callback to LASUI.initUI method
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@5908 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LAS_HOME=las
+LAS_HOME=V7
 CATALINA_HOME=../tomcat
 cp -Ruv WebContent/* ../tomcat/webapps/$LAS_HOME/
 cp -Ruv JavaSource/resources/* $CATALINA_HOME/webapps/$LAS_HOME/WEB-INF/classes/resources/
