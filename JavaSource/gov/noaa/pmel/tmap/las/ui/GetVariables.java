6132d1dc6fead7c74b9fdc28f46d99d67b67388b
Roland Schweitzer <roland.schweitzer@noaa.gov>
2010-10-08 19:17:30 +0000

    Final tweaks to the output to make it look like a getCategories with variables.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@6039 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/las/ui/GetVariables.java
+++ JavaSource/gov/noaa/pmel/tmap/las/ui/GetVariables.java
@@ -4,6 +4,7 @@ import gov.noaa.pmel.tmap.las.exception.LASException;
 import gov.noaa.pmel.tmap.las.jdom.LASConfig;
 import gov.noaa.pmel.tmap.las.product.server.LASConfigPlugIn;
 import gov.noaa.pmel.tmap.las.ui.json.JSONUtil;
+import gov.noaa.pmel.tmap.las.util.Category;
 import gov.noaa.pmel.tmap.las.util.Dataset;
 import gov.noaa.pmel.tmap.las.util.Variable;
 
@@ -23,6 +24,7 @@ import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.jdom.Element;
 import org.jdom.JDOMException;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -64,7 +66,7 @@ public class GetVariables extends Action {
 
 		Dataset dataset = null;
 		try {
-			dataset = lasConfig.getFullDataset(dsID);
+			dataset = lasConfig.getFullDatasetNoGrids(dsID);
 		} catch (JDOMException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -75,10 +77,12 @@ public class GetVariables extends Action {
 
 		
 		StringBuffer xml = new StringBuffer();
-
-		xml.append("<categories><category>");
-		xml.append(dataset.toXML());
-		xml.append("</category></categories>");
+		Element category = new Element("category");
+		category.addContent(dataset.getElement());
+		ArrayList<Category> categories = new ArrayList<Category>();
+		Category cat = new Category(category);
+		categories.add(cat);
+	
 
 		try {
 			PrintWriter respout = response.getWriter();
@@ -87,7 +91,7 @@ public class GetVariables extends Action {
 				respout.print(xml.toString());
 			} else {
 				response.setContentType("application/json");
-				JSONObject json_response = XML.toJSONObject(xml.toString());
+				JSONObject json_response = Util.toJSON(categories, "categories");
 				log.debug(json_response.toString(3));
 				json_response.write(respout);
 			}
