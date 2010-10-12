d1882cc7d5c6322af4250b4e46f9e3396b2bdd69
Roland Schweitzer <roland.schweitzer@noaa.gov>
2010-10-12 14:02:13 +0000

    Get the output from the compile in the right spot.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@6050 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- bin/ol-compile.sh
+++ bin/ol-compile.sh
@@ -2,4 +2,4 @@
 APPDIR=`dirname $0`;
 sed -e 's/The nifty printing utility//' -e "s;<inherits name='br.com.freller.tool.PrintTest'/>;;" -e "s/not needed for this code, but other things in the package need it.//" -e 's;<inherits name="com.google.gwt.maps.GoogleMaps" />;;' < $APPDIR/../src/com/weathertopconsulting/olmapwidget/NativeMapWidget.gwt.xml > native.xml
 mv native.xml $APPDIR/../src/com/weathertopconsulting/olmapwidget/NativeMapWidget.gwt.xml
-java  -Xmx256M -cp "$APPDIR/../src:$APPDIR/../lib/gwtopenlayers.jar:/home/porter/rhs/gwt/gwt-user.jar:/home/porter/rhs/gwt/gwt-dev.jar" com.google.gwt.dev.Compiler -war "$APPDIR/../war/OLMapWidget" "$@" com.weathertopconsulting.olmapwidget.NativeMapWidget;
+java  -Xmx256M -cp "$APPDIR/../src:$APPDIR/../lib/gwtopenlayers.jar:/home/porter/rhs/gwt/gwt-user.jar:/home/porter/rhs/gwt/gwt-dev.jar" com.google.gwt.dev.Compiler -war "$APPDIR/../war/JavaScript/components" "$@" com.weathertopconsulting.olmapwidget.NativeMapWidget;
