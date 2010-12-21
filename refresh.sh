6c5a9b481b101c862d015d2fa29c911bab35c5cf
Jeremy Malczyk <jeremy.malczyk@noaa.gov>
2010-12-21 23:58:05 +0000

    Testing sync
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@11625 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- refresh.sh
+++ refresh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script will copy ferret scripts, velocity templates, html, and javascript to tomcat
 # so you don't have to redeploy. Change the following 2 paramters to reflect your installation.
-WEBAPP_NAME=V7.2
+LAS_NAME=$(pwd | rev | awk -F \/ '{print $1}' | rev)
 CATALINA_HOME=../tomcat
 cp -Ruv WebContent/* ../tomcat/webapps/$LAS_NAME/
-cp -Ruv JavaSource/resources/* $CATALINA_HOME/webapps/$LAS_HOME/WEB-INF/classes/resources/
+cp -Ruv JavaSource/resources/* $CATALINA_HOME/webapps/$LAS_NAME/WEB-INF/classes/resources/
