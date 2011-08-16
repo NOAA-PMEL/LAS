9b789ee91e1b0ffb6362541d8af53c663e1cc195
Heather Koyuk <heather.koyuk@noaa.gov>
2011-08-16 23:53:53 +0000

    Changed the name
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@12571 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ JavaSource/gov/noaa/pmel/tmap/catalogcleaner/CatalogCleaner.java
@@ -0,0 +1,63 @@
+package gov.noaa.pmel.tmap.catalogcleaner;
+
+import gov.noaa.pmel.tmap.catalogcleaner.data.*;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Iterator;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import thredds.catalog.*;
+import thredds.catalog.ThreddsMetadata.Source;
+
+public class CatalogCleaner {
+	private static Logger log = LoggerFactory.getLogger(CatalogCleaner.class);
+	
+	public static void main(String[] args) throws Exception{
+		CatalogCleaner p = new CatalogCleaner();
+		p.run(args[0]);
+	}
+
+	public void run(String uri) throws Exception{
+		//		XMLReader parser = new SAXParser();
+		//		parser.parse(uri);
+		InvCatalog rawT = (InvCatalog) (Utils.FACTORY.readXML(uri));
+		StringBuilder buff = new StringBuilder();
+		if (!uri.startsWith("http://")) {
+			System.out.println("Summary, " + uri+ ", will not clean relative catalogs.");
+		} else
+		if (!rawT.check(buff, false)) {
+			log.error("Invalid catalog " + uri + "\n" + buff.toString(), 1);
+			System.out.println("Summary, " + uri + ", invalid");
+		} else {
+			log.info("Cleaning: " + uri, 0);
+			//			CatalogCleaner cleaner = null;
+			//			try {
+			//				cleaner = new CatalogCleaner(catalog, aggregations, refs, skip, stop_string);
+			//				CCUtils.AGGREGATE = aggregations;
+			//				CCUtils.REFS = refs;
+			//				CCUtils.STOPSTRING = stop_string;
+			//				CCUtils.SKIP = skip;
+			//				CCUtils.KEY = CCUtils.MD5Encode(raw.getUriString());
+
+			//			} catch (UnsupportedEncodingException e) {
+			//				e.printStackTrace();
+			//			}
+
+			
+		//}
+		DataAccess.init(this);
+		// todo: remove from database if exists
+		Crawler crawler = new Crawler();
+		int catalogId = crawler.crawlNew(rawT);
+		Writer ccwriter = new Writer();
+		ccwriter.run(catalogId);
+	}
+	}
+
+}
