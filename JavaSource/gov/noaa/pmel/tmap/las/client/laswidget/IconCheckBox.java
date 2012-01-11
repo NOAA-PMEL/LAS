a43a16958d5d851cde040956fbb29cc877c353f2
rhs <rhs@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2012-01-11 23:42:57 +0000

    Icon widget first draft.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@13182 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ JavaSource/gov/noaa/pmel/tmap/las/client/laswidget/IconCheckBox.java
@@ -0,0 +1,30 @@
+package gov.noaa.pmel.tmap.las.client.laswidget;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
+
+public class IconCheckBox extends Composite {
+	
+	FlexTable layout = new FlexTable();
+	Image icon;
+	CheckBox checkbox;
+	Label label;
+	
+	String id;
+	
+	public IconCheckBox(int i, String id) {
+		this.id = id;
+		checkbox = new CheckBox();
+		String url = GWT.getModuleBaseURL()+"../images/trajectory_icons/icon"+i+".gif";
+		icon = new Image(url);
+		label = new Label(id);
+		layout.setWidget(0, 0, checkbox);
+		layout.setWidget(0, 1, icon);
+		layout.setWidget(0, 2, label);
+		initWidget(layout);
+	}
+}
