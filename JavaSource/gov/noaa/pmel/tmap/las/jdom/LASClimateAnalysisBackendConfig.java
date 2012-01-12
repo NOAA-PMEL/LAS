947ee7e05a6a1b6a40f87c4be15ed2e0afcf50a0
rhs <rhs@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2012-01-12 01:49:18 +0000

    The config class for the Climate Analysis product.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@13185 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ JavaSource/gov/noaa/pmel/tmap/las/jdom/LASClimateAnalysisBackendConfig.java
@@ -0,0 +1,13 @@
+package gov.noaa.pmel.tmap.las.jdom;
+
+
+public class LASClimateAnalysisBackendConfig extends LASBackendConfig {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -785350506999107980L;
+  
+    
+ 
+}
