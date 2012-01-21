ed1739e28247388a13e53c38f5f074d5391de3eb
Roland Schweitzer <roland.schweitzer@noaa.gov>
2012-01-21 19:33:37 +0000

    Refactor for use with addxml.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@13253 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ JavaSource/gov/noaa/pmel/tmap/las/jdom/LASJDOMUtils.java
@@ -0,0 +1,25 @@
+package gov.noaa.pmel.tmap.las.jdom;
+
+import gov.noaa.pmel.tmap.las.util.Category;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.jdom.Element;
+import org.jdom.JDOMException;
+
+public class LASJDOMUtils {
+	public static ArrayList<Category> getCategories(String catxml) throws IOException, JDOMException {
+		LASDocument catdoc = new LASDocument();
+		ArrayList<Category> cats = new ArrayList<Category>();
+		JDOMUtils.XML2JDOM(catxml, catdoc);
+		List catElements = catdoc.getRootElement().getChildren("category");
+		for (Iterator catIt = catElements.iterator(); catIt.hasNext();) {
+			Element catE = (Element) catIt.next();
+			cats.add(new Category(catE));		
+		}
+		return cats;
+	}
+}
