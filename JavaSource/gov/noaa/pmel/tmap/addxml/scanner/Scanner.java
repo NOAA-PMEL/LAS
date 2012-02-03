a9368ee5aacc0265be0370c3031ba5bee9dcbb5a
rhs <rhs@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2012-02-03 03:09:18 +0000

    Build categories using regular expressions on the dataset IDs.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@13286 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/addxml/scanner/Scanner.java
+++ JavaSource/gov/noaa/pmel/tmap/addxml/scanner/Scanner.java
@@ -5,11 +5,14 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
 import gov.noaa.pmel.tmap.addxml.ADDXMLProcessor;
 import gov.noaa.pmel.tmap.addxml.CatalogRefHandler;
+import gov.noaa.pmel.tmap.addxml.CategoryBean;
+import gov.noaa.pmel.tmap.addxml.FilterBean;
 import gov.noaa.pmel.tmap.addxml.addXML;
 import gov.noaa.pmel.tmap.las.jdom.LASDocument;
 
@@ -45,6 +48,9 @@ public class Scanner {
 	        CommandLine line = parser.parse( scannerOptions, args );
 	        String scan = line.getOptionValue("scan");
 	        String regex = line.getOptionValue("regex");
+	        boolean esg = line.hasOption("esg");
+	        String catoption = line.getOptionValue("catregex");
+	        String[] catregex = catoption.split(",");
 	        boolean noregex = false;
 	        if ( regex == null || regex.equals("") ) noregex = true;
 	        if ( scan == null ) {
@@ -69,8 +75,14 @@ public class Scanner {
 			HashMap<String, String> addxmlOptions = new HashMap<String, String>();
 			addxmlOptions.put("force", "t");
 			addxmlOptions.put("oneDataset", "true");
-			addxmlOptions.put("category", "false");
-			addxmlOptions.put("esg", "true");
+			addxmlOptions.put("category", "true");
+			addxmlOptions.put("esg", String.valueOf(esg));
+			Map<String, CategoryBean> cats = new HashMap<String, CategoryBean>();
+			for (int i = 0; i < catregex.length; i++) {
+				CategoryBean cb = new CategoryBean();
+				cb.setName(catregex[i]);
+				cats.put(catregex[i], cb);
+			}
 			addxml.setOptions(addxmlOptions);
 			for (Iterator catIt = catalogs.keySet().iterator(); catIt.hasNext();) {
 				catalogName = (String) catIt.next();
@@ -89,6 +101,21 @@ public class Scanner {
 					if ( noregex || Pattern.matches(regex, catalogURL)) {
 						InvCatalog catalog = (InvCatalog) catfactory.readXML(catalogURL);
 						LASDocument las = (LASDocument) addxml.createXMLfromTHREDDSCatalog(catalog);
+						List childs = las.getRootElement().getChild("datasets").getChildren();
+						if ( childs.size() > 0 ) {
+							Element d  = (Element) childs.get(0);
+							for ( int i = 0; i < catregex.length; i++ ) {
+								if ( Pattern.matches(".*"+catregex[i]+".*", d.getName())) {
+									CategoryBean c = new CategoryBean();
+									c.setName(d.getAttributeValue("name"));
+									FilterBean f = new FilterBean();
+									f.setAction("apply-dataset");
+									f.setContainstag(d.getName());
+									c.addFilter(f);
+									cats.get(catregex[i]).addCategory(c);
+								}
+							}
+						}
 						// I just want the datasets, grids and axes stubs written to a file...
 						File file = new File(filename);
 						las.writeElement("datasets", file, false);
@@ -96,7 +123,15 @@ public class Scanner {
 						las.writeElement("axes", file, true);
 					}
 				}
+				
 			}
+			Element lc = new Element("las_categories");
+			for (Iterator catsIt = cats.keySet().iterator(); catsIt.hasNext();) {
+				String key = (String) catsIt.next();
+				CategoryBean c = cats.get(key);
+				lc.addContent(c.toXml());
+			}
+			ADDXMLProcessor.outputXML("LAS-Categories.xml", lc, true);
 	    } catch( ParseException exp ) {
 	        System.err.println( "Argument parsing failed.  Reason: " + exp.getMessage() );
 	    } catch (ParserConfigurationException e) {
--- JavaSource/gov/noaa/pmel/tmap/addxml/scanner/ScannerOptions.java
+++ JavaSource/gov/noaa/pmel/tmap/addxml/scanner/ScannerOptions.java
@@ -16,6 +16,16 @@ public class ScannerOptions extends Options {
 		                                   .hasArg()
 		                                   .withDescription("Only parse catalogRefs whose URL matches this regular expression")
 		                                   .create("regex");
+		Option esg = OptionBuilder.withArgName("Use ESG")
+		                          .hasArg(false)
+		                          .withDescription("Scan as an ESG THREDDS catalog.")
+		                          .create("esg");
+		Option categories = OptionBuilder.withArgName("Category Regex")
+		                                 .hasArg(true)
+		                                 .withDescription("A comma separated list of regular expression to use to make categories")
+		                                 .create("catregex");
+		this.addOption(categories);
+		this.addOption(esg);
 		this.addOption(scan);
 		this.addOption(catalogRegex);
 	}
