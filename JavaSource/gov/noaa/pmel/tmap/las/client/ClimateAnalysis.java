74d0fa9113c0960e09857198925e56ae9fb6f7cc
Roland Schweitzer <roland.schweitzer@noaa.gov>
2012-02-13 17:19:41 +0000

    Add a matrix for selecting the region and the variables in the time averaged spectrum climate analysis.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@13309 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/las/client/ClimateAnalysis.java
+++ JavaSource/gov/noaa/pmel/tmap/las/client/ClimateAnalysis.java
@@ -1,8 +1,5 @@
 package gov.noaa.pmel.tmap.las.client;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import gov.noaa.pmel.tmap.las.client.laswidget.DatasetFilter;
 import gov.noaa.pmel.tmap.las.client.laswidget.DatasetWidget;
 import gov.noaa.pmel.tmap.las.client.laswidget.DateTimeWidget;
@@ -12,6 +9,14 @@ import gov.noaa.pmel.tmap.las.client.serializable.DatasetSerializable;
 import gov.noaa.pmel.tmap.las.client.serializable.GridSerializable;
 import gov.noaa.pmel.tmap.las.client.serializable.VariableSerializable;
 import gov.noaa.pmel.tmap.las.client.util.Util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ChangeEvent;
@@ -23,6 +28,12 @@ import com.google.gwt.event.logical.shared.SelectionHandler;
 import com.google.gwt.http.client.URL;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.FlexTable.FlexCellFormatter;
+import com.google.gwt.user.client.ui.HTMLTable.ColumnFormatter;
+import com.google.gwt.user.client.ui.HTMLTable.RowFormatter;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.PushButton;
@@ -48,13 +59,15 @@ public class ClimateAnalysis implements EntryPoint {
 	String xSecondVarID = xInitialId;
 	String xTlo;
 	String xThi;
-	String[] panelIDs = {"DatasetSelection", "AnalysisSelection", "DateSelection", "DateSelection", "SubmitSelection"};
-	String[] selectionIDs = {"DatasetWidget", "AnalysisType", "DateRange", "DateRange2", "Submit"};
+	String[] panelIDs = {"DatasetSelection", "AnalysisSelection", "RegionSelection", "DateSelection", "DateSelection", "SubmitSelection"};
+	String[] selectionIDs = {"DatasetWidget", "AnalysisType", "Regions", "DateRange", "DateRange2", "Submit"};
 	String highLightStyle = "highlightSelection";
 	String selectionStyle = "selection";
 	Set<Integer> set = new HashSet<Integer>();
+	PacificRegionsByVariable xRegions = new PacificRegionsByVariable();
 	@Override
 	public void onModuleLoad() {	
+	
 		set.clear();
 		xSubmit.addClickHandler(xSubmitClick);
 		xSubmit.setWidth("80px");
@@ -71,6 +84,7 @@ public class ClimateAnalysis implements EntryPoint {
 			
 		});
 		RootPanel.get("AnalysisType").add(xAnalysisType);
+		RootPanel.get("Regions").add(xRegions);
 		xDatasetWidget.addFilter(cimp5filter);
 		String xml = Util.getParameterString("xml");
 	    xFirstName = Util.getParameterString("dsname");
@@ -85,8 +99,8 @@ public class ClimateAnalysis implements EntryPoint {
 
 			@Override
 			public void onChange(ChangeEvent arg0) {
-				set.add(2);
-				setBackground(3);
+				set.add(3);
+				setBackground(4);
 			}
 			
 		});
@@ -94,8 +108,8 @@ public class ClimateAnalysis implements EntryPoint {
 
 			@Override
 			public void onChange(ChangeEvent arg0) {
-				set.add(3);
-				setBackground(4);	
+				set.add(4);
+				setBackground(5);	
 			}
 			
 		});
@@ -236,4 +250,423 @@ public class ClimateAnalysis implements EntryPoint {
 			}
 		}
 	}
+	private class PacificRegionsByVariable extends Composite {
+		FlexTable layout = new FlexTable();
+		// Pacific
+		CheckBox ts_cb11 = new CheckBox("Nino3");
+		CheckBox ts_cb12 = new CheckBox("Nino4");
+		CheckBox ts_cb13 = new CheckBox("Nino34");
+		CheckBox ts_cb14 = new CheckBox("Nino12");
+		// Atlantic
+		CheckBox ts_cb18 = new CheckBox("EqAtl");
+		CheckBox ts_cb19 = new CheckBox("TNAtl");
+		CheckBox ts_cb110 = new CheckBox("TSAtl");
+		// Indian
+		CheckBox ts_cb111 = new CheckBox("EEqIO");
+		CheckBox ts_cb112 = new CheckBox("WEqIO");
+		
+		// Pacific
+		CheckBox tauu_cb21 = new CheckBox("Nino3");
+		CheckBox tauu_cb22 = new CheckBox("Nino4");
+		CheckBox tauu_cb25 = new CheckBox("WEqPac");
+		// Atlantic
+		CheckBox tauu_cb28 = new CheckBox("EqAtl");
+		// Indian
+		CheckBox tauu_cb213 = new CheckBox("EqIO");
+		
+		// Pacific
+		CheckBox ps_cb36 = new CheckBox("Darwin");
+		CheckBox ps_cb37 = new CheckBox("Tahiti");
+		
+		List<CheckBox> ts = new ArrayList<CheckBox>();
+		List<CheckBox> tauu = new ArrayList<CheckBox>();
+		List<CheckBox> ps = new ArrayList<CheckBox>();
+		
+		List<CheckBox> pacific = new ArrayList<CheckBox>();
+		List<CheckBox> atlantic = new ArrayList<CheckBox>();
+		List<CheckBox> indian = new ArrayList<CheckBox>();
+		
+		CheckBox cb_all = new CheckBox("all");
+		CheckBox cb_ts = new CheckBox("ts");
+		CheckBox cb_tauu = new CheckBox("tauu");
+		CheckBox cb_ps = new CheckBox("ps");
+		
+		List<CheckBox> variables = new ArrayList<CheckBox>();
+		List<CheckBox> regions = new ArrayList<CheckBox>();
+		
+		CheckBox cb_tp = new CheckBox("Tropical Pacific");
+		CheckBox cb_ta = new CheckBox("Tropical Atlantic");
+		CheckBox cb_ti = new CheckBox("Tropical Indian");
+		
+		public PacificRegionsByVariable () {
+			FlexCellFormatter formatter = layout.getFlexCellFormatter();
+			formatter.setColSpan(0, 1, 7);
+			formatter.setColSpan(0, 2, 3);
+			formatter.setColSpan(0, 3, 3);
+			
+			ts_cb11.addClickHandler(checkPacific);
+			ts_cb11.addClickHandler(checkTS);
+			ts_cb12.addClickHandler(checkPacific);
+			ts_cb12.addClickHandler(checkTS);
+			ts_cb13.addClickHandler(checkPacific);
+			ts_cb13.addClickHandler(checkTS);
+			ts_cb14.addClickHandler(checkPacific);
+			ts_cb14.addClickHandler(checkTS);
+			
+			ts_cb18.addClickHandler(checkAtlantic);
+			ts_cb18.addClickHandler(checkTS);
+			ts_cb19.addClickHandler(checkAtlantic);
+			ts_cb19.addClickHandler(checkTS);
+			ts_cb110.addClickHandler(checkAtlantic);
+			ts_cb110.addClickHandler(checkTS);
+			
+			ts_cb111.addClickHandler(checkIndian);
+			ts_cb111.addClickHandler(checkTS);
+			ts_cb112.addClickHandler(checkIndian);
+			ts_cb112.addClickHandler(checkTS);
+			
+			tauu_cb21.addClickHandler(checkPacific);
+			tauu_cb21.addClickHandler(checkTAUU);
+			tauu_cb22.addClickHandler(checkPacific);
+			tauu_cb22.addClickHandler(checkTAUU);
+			tauu_cb25.addClickHandler(checkPacific);
+			tauu_cb25.addClickHandler(checkTAUU);
+			// Atlantic
+			tauu_cb28.addClickHandler(checkAtlantic);
+			tauu_cb28.addClickHandler(checkTAUU);
+			// Indian
+			tauu_cb213.addClickHandler(checkIndian);
+			tauu_cb213.addClickHandler(checkTAUU);
+
+			
+			ps_cb36.addClickHandler(checkPacific);
+			ps_cb36.addClickHandler(checkPS);
+			ps_cb37.addClickHandler(checkPacific);
+			ps_cb37.addClickHandler(checkPS);
+			
+			cb_tp.addClickHandler(new ClickHandler(){
+
+				@Override
+				public void onClick(ClickEvent event) {
+					boolean v = cb_tp.getValue();
+					for (Iterator pacIt = pacific.iterator(); pacIt.hasNext();) {
+						CheckBox cb = (CheckBox) pacIt.next();
+						cb.setValue(v);
+					}
+					checkAll();
+				}
+				
+			});
+			
+			cb_ta.addClickHandler(new ClickHandler(){
+
+				@Override
+				public void onClick(ClickEvent event) {
+					boolean v = cb_ta.getValue();
+					for (Iterator atlIt = atlantic.iterator(); atlIt.hasNext();) {
+						CheckBox cb = (CheckBox) atlIt.next();
+						cb.setValue(v);
+					}
+					checkAll();
+				}
+				
+			});
+			
+			cb_ti.addClickHandler(new ClickHandler() {
+
+				@Override
+				public void onClick(ClickEvent event) {
+					boolean v = cb_ti.getValue();
+					for (Iterator inIt = indian.iterator(); inIt.hasNext();) {
+						CheckBox cb = (CheckBox) inIt.next();
+						cb.setValue(v);
+					}
+					checkAll();
+				}
+				
+			});
+			
+			cb_ts.addClickHandler(new ClickHandler() {
+
+				@Override
+				public void onClick(ClickEvent event) {
+					boolean v = cb_ts.getValue();
+					for (Iterator tsIt = ts.iterator(); tsIt.hasNext();) {
+						CheckBox cb = (CheckBox) tsIt.next();
+						cb.setValue(v);
+					}
+					checkAll();
+				}
+				
+			});
+			
+			cb_tauu.addClickHandler(new ClickHandler() {
+
+				@Override
+				public void onClick(ClickEvent event) {
+					boolean v = cb_tauu.getValue();
+					for (Iterator tauuIt = tauu.iterator(); tauuIt.hasNext();) {
+						CheckBox cb = (CheckBox) tauuIt.next();
+						cb.setValue(v);
+					}
+					checkAll();
+				}
+				
+			});
+			
+			
+			cb_ps.addClickHandler(new ClickHandler() {
+
+				@Override
+				public void onClick(ClickEvent event) {
+					boolean v = cb_ps.getValue();
+					for (Iterator psIt = ps.iterator(); psIt.hasNext();) {
+						CheckBox cb = (CheckBox) psIt.next();
+						cb.setValue(v);
+					}
+					checkAll();
+				}
+				
+			});
+			
+			cb_all.addClickHandler(new ClickHandler() {
+
+				@Override
+				public void onClick(ClickEvent event) {
+					boolean v = cb_all.getValue();
+					for (Iterator tsIt = ts.iterator(); tsIt.hasNext();) {
+						CheckBox cb = (CheckBox) tsIt.next();
+						cb.setValue(v);
+					}
+					for (Iterator tauuIt = tauu.iterator(); tauuIt.hasNext();) {
+						CheckBox cb = (CheckBox) tauuIt.next();
+						cb.setValue(v);
+					}
+					for (Iterator psIt = ps.iterator(); psIt.hasNext();) {
+						CheckBox cb = (CheckBox) psIt.next();
+						cb.setValue(v);
+					}
+					for (Iterator regionsIt = regions.iterator(); regionsIt.hasNext();) {
+						CheckBox cb = (CheckBox) regionsIt.next();
+						cb.setValue(v);
+					}
+					for (Iterator variablesIt = variables.iterator(); variablesIt.hasNext();) {
+						CheckBox cb = (CheckBox) variablesIt.next();
+						cb.setValue(v);
+					}
+				}
+				
+			});
+			
+			
+			ts.add(ts_cb11);
+			ts.add(ts_cb12);
+			ts.add(ts_cb13);
+			ts.add(ts_cb14);
+			ts.add(ts_cb18);
+			ts.add(ts_cb19);
+			ts.add(ts_cb110);
+			ts.add(ts_cb111);
+			ts.add(ts_cb112);
+			
+			tauu.add(tauu_cb21);
+			tauu.add(tauu_cb22);
+			tauu.add(tauu_cb25);
+			tauu.add(tauu_cb28);
+			tauu.add(tauu_cb213);
+			
+			ps.add(ps_cb36);
+			ps.add(ps_cb37);
+			
+			pacific.add(ts_cb11);
+			pacific.add(ts_cb12);
+			pacific.add(ts_cb13);
+			pacific.add(ts_cb14);
+			pacific.add(tauu_cb21);
+			pacific.add(tauu_cb22);
+			pacific.add(tauu_cb25);
+			pacific.add(ps_cb36);
+			pacific.add(ps_cb37);
+			
+			atlantic.add(ts_cb18);
+			atlantic.add(ts_cb19);
+			atlantic.add(ts_cb110);
+			atlantic.add(tauu_cb28);
+			
+			indian.add(ts_cb111);
+			indian.add(ts_cb112);
+			indian.add(tauu_cb213);
+			
+			variables.add(cb_ts);
+			variables.add(cb_tauu);
+			variables.add(cb_ps);
+			
+			regions.add(cb_tp);
+			regions.add(cb_ta);
+			regions.add(cb_ti);
+			
+			layout.setWidget(0, 0, cb_all);
+			layout.setWidget(0, 1, cb_tp);
+			layout.setWidget(0, 2, cb_ta);
+			layout.setWidget(0, 3, cb_ti);
+			
+				
+			layout.setWidget(1, 0, cb_ts);
+			layout.setWidget(1, 1, ts_cb11);
+			layout.setWidget(1, 2, ts_cb12);
+			layout.setWidget(1, 3, ts_cb13);
+			layout.setWidget(1, 4, ts_cb14);
+			layout.setWidget(1, 8, ts_cb18);
+			layout.setWidget(1, 9, ts_cb19);
+			layout.setWidget(1, 10, ts_cb110);
+			layout.setWidget(1, 11, ts_cb111);
+			layout.setWidget(1, 12, ts_cb112);
+			
+			layout.setWidget(2, 0, cb_tauu);
+			layout.setWidget(2, 1, tauu_cb21);
+			layout.setWidget(2, 2, tauu_cb22);
+			layout.setWidget(2, 5, tauu_cb25);
+			layout.setWidget(2, 8, tauu_cb28);
+			layout.setWidget(2, 13, tauu_cb213);
+			
+			layout.setWidget(3, 0, cb_ps);
+			layout.setWidget(3, 6, ps_cb36);
+			layout.setWidget(3, 7, ps_cb37);
+			
+			initWidget(layout);
+			
+		}
+		ClickHandler checkTS = new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				boolean v = true;
+				for (Iterator tsIt = ts.iterator(); tsIt.hasNext();) {
+					CheckBox cb = (CheckBox) tsIt.next();
+					v = v && cb.getValue();
+				}
+				cb_ts.setValue(v);
+				checkAll();
+			}
+			
+		};
+		ClickHandler checkTAUU = new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				boolean v = true;
+				for (Iterator tauuIt = tauu.iterator(); tauuIt.hasNext();) {
+					CheckBox cb = (CheckBox) tauuIt.next();
+					v = v && cb.getValue();
+				}
+				cb_tauu.setValue(v);
+				checkAll();
+			}
+			
+		};
+		ClickHandler checkPS = new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				boolean v = true;
+				for (Iterator tauuIt = ps.iterator(); tauuIt.hasNext();) {
+					CheckBox cb = (CheckBox) tauuIt.next();
+					v = v && cb.getValue();
+				}
+				cb_ps.setValue(v);
+				checkAll();
+			}
+			
+		};
+		ClickHandler checkPacific = new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				checkPacific();
+				checkAll();
+
+			}
+		};
+		private void checkPacific() {
+			boolean v = true;
+			for (Iterator pacIt = pacific.iterator(); pacIt.hasNext();) {
+				CheckBox cb = (CheckBox) pacIt.next();
+				v = v && cb.getValue();
+			}
+			cb_tp.setValue(v);
+		}
+		ClickHandler checkAtlantic = new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				checkAtlantic();
+				checkAll();
+			}
+			
+		};
+		private void checkAtlantic() {
+			boolean v = true;
+			for (Iterator atlIt = atlantic.iterator(); atlIt.hasNext();) {
+				CheckBox cb = (CheckBox) atlIt.next();
+				v = v && cb.getValue();
+			}
+			cb_ta.setValue(v);
+			
+		}
+		ClickHandler checkIndian = new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				checkIndian();
+				checkAll();
+			}
+			
+		};
+		private void checkIndian() {
+			boolean v = true;
+			for (Iterator inIt = indian.iterator(); inIt.hasNext();) {
+				CheckBox cb = (CheckBox) inIt.next();
+				v = v && cb.getValue();
+			}
+			cb_ti.setValue(v);
+		}
+		private void checkAll() {
+			set.add(2);
+			setBackground(3);
+			boolean v = true;
+			boolean subv = true;
+			for (Iterator tsIt = ts.iterator(); tsIt.hasNext();) {
+				CheckBox cb = (CheckBox) tsIt.next();
+				subv = subv && cb.getValue();
+			}
+			cb_ts.setValue(subv);
+			v = v && subv;
+			subv = true;
+			for (Iterator tauuIt = tauu.iterator(); tauuIt.hasNext();) {
+				CheckBox cb = (CheckBox) tauuIt.next();
+				subv = subv && cb.getValue();
+			}
+			cb_tauu.setValue(subv);
+			v = v && subv;
+			subv = true;
+			for (Iterator psIt = ps.iterator(); psIt.hasNext();) {
+				CheckBox cb = (CheckBox) psIt.next();
+				subv = subv && cb.getValue();
+			}
+			cb_ps.setValue(subv);
+			v = v && subv;
+			checkPacific();
+			checkAtlantic();
+			checkIndian();
+			for (Iterator regionsIt = regions.iterator(); regionsIt.hasNext();) {
+				CheckBox cb = (CheckBox) regionsIt.next();
+				v = v && cb.getValue();
+			}
+			for (Iterator variablesIt = variables.iterator(); variablesIt.hasNext();) {
+				CheckBox cb = (CheckBox) variablesIt.next();
+				v = v && cb.getValue();
+			}
+			cb_all.setValue(v);
+		}
+	};
 }
