6520203da314e9b0e6e8247f6521998c57f6db2f
Roland Schweitzer <roland.schweitzer@noaa.gov>
2012-04-10 13:39:15 +0000

    Do a pop up when the KML load starts and remove it when it finished.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@13703 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/las/client/OpenLayersKML.java
+++ JavaSource/gov/noaa/pmel/tmap/las/client/OpenLayersKML.java
@@ -1,6 +1,8 @@
 package gov.noaa.pmel.tmap.las.client;
 
+import gov.noaa.pmel.tmap.las.client.map.MapSelectionChangeListener;
 import gov.noaa.pmel.tmap.las.client.map.OLMapWidget;
+import gov.noaa.pmel.tmap.las.client.util.URLUtil;
 
 import org.gwtopenmaps.openlayers.client.Icon;
 import org.gwtopenmaps.openlayers.client.LonLat;
@@ -8,6 +10,10 @@ import org.gwtopenmaps.openlayers.client.Pixel;
 import org.gwtopenmaps.openlayers.client.Size;
 import org.gwtopenmaps.openlayers.client.control.SelectFeature;
 import org.gwtopenmaps.openlayers.client.event.FeatureHighlightedListener;
+import org.gwtopenmaps.openlayers.client.event.LayerLoadEndListener;
+import org.gwtopenmaps.openlayers.client.event.LayerLoadStartListener;
+import org.gwtopenmaps.openlayers.client.event.LayerLoadEndListener.LoadEndEvent;
+import org.gwtopenmaps.openlayers.client.event.LayerLoadStartListener.LoadStartEvent;
 import org.gwtopenmaps.openlayers.client.feature.VectorFeature;
 import org.gwtopenmaps.openlayers.client.format.KML;
 import org.gwtopenmaps.openlayers.client.layer.GML;
@@ -31,6 +37,9 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.maps.client.overlay.MarkerOptions;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.PushButton;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
@@ -42,6 +51,10 @@ public class OpenLayersKML implements EntryPoint {
 	PushButton kmlButton = new PushButton("Load KML");
 	HorizontalPanel header = new HorizontalPanel();
 	VerticalPanel panel = new VerticalPanel();
+	PopupPanel load = new PopupPanel();
+	HorizontalPanel loadPanel = new HorizontalPanel();
+	Image spinImage;
+	Label loadLabel = new Label("   Please wait while the KML loads.  This make take a couple of minutes...");
 	@Override
 	public void onModuleLoad() {
 		
@@ -49,6 +62,11 @@ public class OpenLayersKML implements EntryPoint {
 		map.setTool("xy");
         map.setDataExtent(-90, 90, -180, 180);
     	kmlURL.setValue("http://gazelle.weathertopconsulting.com:8282/baker/output/4D8EDF082029CB91B161DD9A12D198D7_kml.kml");
+    	spinImage = new Image(URLUtil.getImageURL()+"/mozilla_blu.gif");
+		spinImage.setSize("18px", "18px");
+		loadPanel.add(spinImage);
+		loadPanel.add(loadLabel);
+		load.add(loadPanel);
 		kmlButton.addClickHandler(new ClickHandler() {
 
 			@Override
@@ -69,6 +87,23 @@ public class OpenLayersKML implements EntryPoint {
 				options.setProtocol(httpProt);
 				options.setIsBaseLayer(false);
 				Vector v = new Vector("KML", options);
+				v.addLayerLoadStartListener(new LayerLoadStartListener() {
+					
+					@Override
+					public void onLoadStart(LoadStartEvent eventObject) {
+						int left = map.getAbsoluteLeft() + (map.getOffsetWidth()/2) - (load.getOffsetWidth()/2);
+						int top = map.getAbsoluteTop() + (map.getOffsetHeight()/2) - (load.getOffsetHeight()/2);
+						load.setPopupPosition(left, top);
+						load.show();
+					}
+				});
+				v.addLayerLoadEndListener(new LayerLoadEndListener() {
+					
+					@Override
+					public void onLoadEnd(LoadEndEvent eventObject) {
+						load.hide();
+					}
+				});
 				SelectFeature selectFeature = new SelectFeature(v);
 			
 				selectFeature.addFeatureHighlightedListener(new FeatureHighlightedListener() {
@@ -93,6 +128,15 @@ public class OpenLayersKML implements EntryPoint {
 				selectFeature.activate();
 			}
 			
+		});
+		map.setMapListener(new MapSelectionChangeListener() {
+			
+			@Override
+			public void onFeatureChanged() {
+				
+				map.zoomMapToSelection();
+				
+			}
 		});
 		header.add(kmlURL);
 		header.add(kmlButton);
