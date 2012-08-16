5520b8a0efc8daee211505ff6b9ee398e066b3a0
rhs <rhs@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2012-08-16 16:07:31 +0000

    Allow error=true.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@14725 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/las/filter/RequestInputFilter.java
+++ JavaSource/gov/noaa/pmel/tmap/las/filter/RequestInputFilter.java
@@ -128,6 +128,7 @@ public class RequestInputFilter implements Filter {
     	"cancel",
     	"debug",
     	"stream",
+    	"error",
     	"auto"
     };
     private final static Set<String> LAS_BOOLEAN_PARAMETERS = new HashSet<String>(Arrays.asList(bp));
