c847b7ab05449b7f0d8316b212a0a3358adf18f5
Kofi Weusijana <kofi.weusijana@noaa.gov>
2012-09-11 18:48:02 +0000

    #1356: Improved Help menu look and feel Weusijana
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@14827 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ JavaSource/gov/noaa/pmel/tmap/las/client/ui/AutoCloseMenuBar.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package gov.noaa.pmel.tmap.las.client.ui;
+
+import java.util.logging.Logger;
+
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
+import com.google.gwt.user.client.Window;
+
+/**
+ * Extends the standard menu bar widget. Adds the behavior of closing an open menu when the user resizes the Web browser window.
+ * @author weusijana
+ *
+ */
+public class AutoCloseMenuBar extends com.google.gwt.user.client.ui.MenuBar {
+
+    private final class CloseChildrenResizeHandler implements ResizeHandler {
+		@Override
+		public void onResize(ResizeEvent event) {
+		    logger.info("onResize(ResizeEvent event) started with event:"+event.toDebugString());
+		    closeAllChildren(false);
+		}
+	}
+
+	private final Logger logger = Logger.getLogger(AutoCloseMenuBar.class.getName());
+
+    /**
+	 * 
+	 */
+	public AutoCloseMenuBar() {
+		super();
+        closeChildrenOnResize();
+	}
+
+	/**
+	 * @param vertical
+	 */
+	public AutoCloseMenuBar(boolean vertical) {
+		super(vertical);
+        closeChildrenOnResize();
+	}
+
+	/**
+	 * @param vertical
+	 * @param resources
+	 */
+	public AutoCloseMenuBar(boolean vertical, Resources resources) {
+		super(vertical, resources);
+        closeChildrenOnResize();
+	}
+
+	/**
+	 * @param resources
+	 */
+	public AutoCloseMenuBar(Resources resources) {
+		super(resources);
+        closeChildrenOnResize();
+	}
+
+	/**
+	 * 
+	 */
+	private void closeChildrenOnResize() {
+		Window.addResizeHandler(new CloseChildrenResizeHandler());
+	}
+
+}
