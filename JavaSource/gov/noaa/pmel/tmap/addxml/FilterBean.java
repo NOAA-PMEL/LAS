8bf598126fb415b296e5341b4eeb827ee99dd904
rhs <rhs@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2012-12-17 22:59:18 +0000

    Add equals-tag as an option in the category filter.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@15242 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/addxml/FilterBean.java
+++ JavaSource/gov/noaa/pmel/tmap/addxml/FilterBean.java
@@ -16,62 +16,71 @@ import org.jdom.*;
  * @version 1.0
  */
 public class FilterBean {
-  private String action;
-  private String contains;
-  private String equals;
-  private String containstag;
-  public FilterBean() {
-  }
+	private String action;
+	private String contains;
+	private String equals;
+	private String containstag;
+	private String equalstag;
+	public FilterBean() {
+	}
 
-  public void setAction(String action) {
-    this.action = action;
-  }
+	public void setAction(String action) {
+		this.action = action;
+	}
 
-  public void setContains(String contains) {
-    this.contains = contains;
-  }
+	public void setContains(String contains) {
+		this.contains = contains;
+	}
 
-  public void setEquals(String equals) {
-    this.equals = equals;
-  }
+	public void setEquals(String equals) {
+		this.equals = equals;
+	}
 
-  public void setContainstag(String containstag) {
-    this.containstag = containstag;
-  }
+	public void setContainstag(String containstag) {
+		this.containstag = containstag;
+	}
 
-  public String getAction() {
-    return action;
-  }
+	public String getAction() {
+		return action;
+	}
 
-  public String getContains() {
-    return contains;
-  }
+	public String getContains() {
+		return contains;
+	}
 
-  public String getEquals() {
-    return equals;
-  }
+	public String getEquals() {
+		return equals;
+	}
 
-  public String getContainstag() {
-    return containstag;
-  }
+	public String getContainstag() {
+		return containstag;
+	}
 
-  /**
-   * toXml
-   *
-   * @return Element
-   */
-  public Element toXml() {
-    Element filterE = new Element("filter");
-    filterE.setAttribute("action", action);
-    if (contains != null) {
-      filterE.setAttribute("contains", contains);
-    }
-    else if (equals != null) {
-      filterE.setAttribute("equals", equals);
-    }
-    else if ( containstag != null ) {
-      filterE.setAttribute("contains-tag", containstag);
-    }
-    return filterE;
-  }
+	public String getEqualstag() {
+		return equalstag;
+	}
+
+	public void setEqualstag(String equalstag) {
+		this.equalstag = equalstag;
+	}
+
+	/**
+	 * toXml
+	 *
+	 * @return Element
+	 */
+	public Element toXml() {
+		Element filterE = new Element("filter");
+		filterE.setAttribute("action", action);
+		if (contains != null) {
+			filterE.setAttribute("contains", contains);
+		} else if (equals != null) {
+			filterE.setAttribute("equals", equals);
+		} else if ( containstag != null ) {
+			filterE.setAttribute("contains-tag", containstag);
+		} else if ( equalstag != null ) {
+			filterE.setAttribute("equals-tag", equalstag);
+		}
+		return filterE;
+	}
 }
