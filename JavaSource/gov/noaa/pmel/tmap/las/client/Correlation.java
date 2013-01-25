fa027b1b5a751d98250ce3d91ba3f14b185a0d90
Roland Schweitzer <roland.schweitzer@noaa.gov>
2013-01-25 15:58:38 +0000

    Spiffy up the message for old browsers that don't support canvas.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@15381 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/las/client/Correlation.java
+++ JavaSource/gov/noaa/pmel/tmap/las/client/Correlation.java
@@ -761,7 +761,10 @@ public class Correlation implements EntryPoint {
 		if ( frontCanvas != null ) {
 		    frontCanvasContext = frontCanvas.getContext2d();
 		} else {
-		    Window.alert("Your browser does not support drawing on the plot image for zooming.");
+		    Window.alert("You are accessing this site with an older, no longer supported browser.\n"+
+                         "Some or all features of this site will not work correctly using your browser.\n"+
+                         "Recommended browsers include these or higher versions of these:\n"+
+                         "IE 9.0   FF 17.0    Chorme 23.0    Safari 5.1");
 		}
 
 		String url = Util.getProductServer() + "?xml="
