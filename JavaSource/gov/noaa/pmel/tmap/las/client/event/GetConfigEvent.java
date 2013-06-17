6499e7e0794ce3a05ebacb3bfc420ba51f53779d
Roland Schweitzer <roland.schweitzer@noaa.gov>
2013-06-17 22:04:52 +0000

    Use category id for getconfig.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@15967 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/las/client/event/GetConfigEvent.java
+++ JavaSource/gov/noaa/pmel/tmap/las/client/event/GetConfigEvent.java
@@ -4,13 +4,15 @@ import com.google.gwt.event.shared.EventHandler;
 import com.google.gwt.event.shared.GwtEvent;
 
 public class GetConfigEvent extends GwtEvent<GetConfigEvent.Handler> {
+    private String catid;
     private String dsid;
     private String varid;
     public static final Type<GetConfigEvent.Handler> TYPE = new Type<GetConfigEvent.Handler>();
     public interface Handler extends EventHandler {
         void onGetConfig(GetConfigEvent event);
     }
-    public GetConfigEvent(String dsid, String varid) {
+    public GetConfigEvent(String catid, String dsid, String varid) {
+        this.catid = catid;
         this.dsid = dsid;
         this.varid = varid;
     }
@@ -23,6 +25,13 @@ public class GetConfigEvent extends GwtEvent<GetConfigEvent.Handler> {
     protected void dispatch(Handler handler) {
         handler.onGetConfig(this);
         
+    }
+    
+    public String getCatid() {
+        return catid;
+    }
+    public void setCatid(String catid) {
+        this.catid = catid;
     }
     public String getDsid() {
         return dsid;
