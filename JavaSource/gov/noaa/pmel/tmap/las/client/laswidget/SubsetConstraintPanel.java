e21a380a7c36a2858c1f446a06fe3f793cad1d22
rhs <rhs@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2013-09-12 14:26:43 +0000

    Do filter comparisons in lower case, thereby ignoring case.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@16397 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/las/client/laswidget/SubsetConstraintPanel.java
+++ JavaSource/gov/noaa/pmel/tmap/las/client/laswidget/SubsetConstraintPanel.java
@@ -68,7 +68,7 @@ public class SubsetConstraintPanel extends Composite {
                     for (Iterator rIt = currentValues.keySet().iterator(); rIt.hasNext();) {
                         String key_value = (String) rIt.next();
                         String value = (String) currentValues.get(key_value);
-                        if ( value.startsWith(f) ) {
+                        if ( value.toLowerCase().startsWith(f.toLowerCase()) ) {
                             valuesList.addItem(value, key_value);
                         }
                     }
