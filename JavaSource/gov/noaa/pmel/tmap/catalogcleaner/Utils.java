4e977a543861fe4ebfaeab3376f4c94a81ec2e9f
rhs <rhs@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2014-01-27 21:25:55 +0000

    Remove the old catalog cleaner code since it is now in its own package.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@16936 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/catalogcleaner/Utils.java
+++
@@ -1,8 +0,0 @@
-package gov.noaa.pmel.tmap.catalogcleaner;
-
-import thredds.catalog.InvCatalogFactory;
-
-public class Utils {
-	public static InvCatalogFactory FACTORY = new InvCatalogFactory("default", false);
-}
-
