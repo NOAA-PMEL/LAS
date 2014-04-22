b9487f09ae4e575d7bac54fbbe4a5b8680d464b4
rhs <rhs@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2014-04-22 00:33:37 +0000

    A simple main to test pulling down data from ERDDAP.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@17287 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ JavaSource/gov/noaa/pmel/tmap/las/mains/PullFile.java
@@ -0,0 +1,43 @@
+package gov.noaa.pmel.tmap.las.mains;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.httpclient.HttpException;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
+
+import gov.noaa.pmel.tmap.las.ui.LASProxy;
+
+public class PullFile {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        LASProxy lasProxy = new LASProxy();
+        if (args.length <= 0 ) {
+            System.out.println("Add URL to grab to arguments.");
+        }
+        DateTimeFormatter fmt = ISODateTimeFormat.dateTime();
+
+        for (int i = 0; i < args.length; i++) {
+            String dsUrl = args[i];
+            File out = new File("out"+String.valueOf(i));
+            try {
+                DateTime dt = new DateTime();
+                System.out.println("TableDapTool starting file pull for the only file at "+fmt.print(dt));
+                lasProxy.executeGetMethodAndSaveResult(dsUrl, out, null);
+                System.out.println("TableDapTool finishing file pull for the only file at "+fmt.print(dt));
+            } catch (HttpException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
