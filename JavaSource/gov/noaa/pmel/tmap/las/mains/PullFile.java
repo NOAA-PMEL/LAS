74df5bcbdeca2b54d7e68f670998381595a8738b
Roland Schweitzer <roland.schweitzer@noaa.gov>
2014-04-23 14:49:51 +0000

    try half
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@17297 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/las/mains/PullFile.java
+++ JavaSource/gov/noaa/pmel/tmap/las/mains/PullFile.java
@@ -38,7 +38,7 @@ public class PullFile {
                 DateTime dt = new DateTime();
                 System.out.println("TableDapTool starting file pull for the only file at "+fmt.print(dt));
                 String b = "http://dunkel.pmel.noaa.gov:8660/erddap/tabledap/dsg_files_badval_7f9a_0653_3fc1.ncCF";
-                String q = "traj1,QC_flag,cruise_id,cruise_name,vessel_name,PIs,DOI,obs1,fCO2_recomputed,pressure,pressure_ncep_slp,pressure_equilibrium,salinity,salinity_woa5,temperature,temperature_equilibrium,ETOPO2_depth,day_of_year,WOCE_flag,fCO2_source,region_id,data_id,tmonth,lon360&time>=1968-11-16T22:00:00Z&time<=2008-01-01T00:00:00Z&orderBy(\"cruise_expocode,time\")";
+                String q = "traj1,QC_flag,cruise_id,cruise_name,vessel_name,PIs,DOI,obs1,fCO2_recomputed,pressure,pressure_ncep_slp,pressure_equilibrium,salinity,salinity_woa5,temperature,temperature_equilibrium,ETOPO2_depth,day_of_year,WOCE_flag,fCO2_source,region_id,data_id,tmonth,lon360&time>=1968-11-16T22:00:00Z&time<=1988-01-01T00:00:00Z&orderBy(\"cruise_expocode,time\")";
                 q = URLEncoder.encode(q.toString(), "UTF-8").replaceAll("\\+", "%20");
                 lasProxy.executeGetMethodAndSaveResult(b + "?" + q, out, null);
                 dt = new DateTime();
