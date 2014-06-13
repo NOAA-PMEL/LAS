286a943b3ec76a7888434e6597f0911349dbebae
Karl Smith <karl.smith@noaa.gov>
2014-06-13 03:20:51 +0000

    Updated AuthenticationFilter.  Probably will never be used.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@17713 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/las/filter/AuthenticationFilter.java
+++ JavaSource/gov/noaa/pmel/tmap/las/filter/AuthenticationFilter.java
@@ -1,142 +1,153 @@
 package gov.noaa.pmel.tmap.las.filter;
 
+import java.io.IOException;
+import java.security.Principal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-//import oracle.jdbc.rowset.OracleWebRowSet;
-
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-
-import java.security.Principal;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Properties;
 
 
 public class AuthenticationFilter implements Filter {
 	private static Logger log = LogManager.getLogger(AuthenticationFilter.class.getName());
-	protected FilterConfig filterConfig;
+	private String databaseUrl;
+	private String selectUser;
+	private String selectPass;
 
-	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) {
-		log.debug("Authentication: Request received");
-		HttpServletRequest  request = (HttpServletRequest)  servletRequest;
-		HttpServletResponse  response = (HttpServletResponse) servletResponse;
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		log.info("AuthenticationFilter: initializing");
+		// Register the SQL driver - no harm if already registered
+		String driverName = filterConfig.getInitParameter("driverName");
+		if ( driverName == null )
+			throw new ServletException("driverName not defined in the AuthenticationFilter configuration");
+		try {
+			Class.forName(driverName).newInstance();
+		} catch (Exception ex) {
+			throw new ServletException("Unable to register the SQL driver " + 
+					driverName + ": " + ex.getMessage());
+		}
+		//Get the rest of the info for making database queries
+		databaseUrl = filterConfig.getInitParameter("databaseURL");
+		if ( databaseUrl == null )
+			throw new ServletException("databaseURL not defined in the AuthenticationFilter configuration");
+		selectUser = filterConfig.getInitParameter("selectUsername");
+		if ( selectUser == null ) 
+			throw new ServletException("selectUsername not defined in the AuthenticationFilter configuration");
+		selectPass = filterConfig.getInitParameter("selectPassword");
+		if ( selectPass == null ) 
+			throw new ServletException("selectPassword not defined in the AuthenticationFilter configuration");
+		Connection catConn;
+		try {
+			catConn = DriverManager.getConnection(databaseUrl, selectUser, selectPass);
+			if ( catConn == null )
+				throw new ServletException("unable to make a connection to the database for AuthenticationFilter: null returned");
+			catConn.close();
+		} catch (SQLException ex) {
+			throw new ServletException("Unable to make a connection to the database for AuthenticationFilter: " + ex.getMessage());
+		}
+	}
 
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, 
+			FilterChain chain) throws IOException, ServletException {
+		log.info("Authentication request received");
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
+		HttpSession session = request.getSession(true);
+
+		// Check if we have already authenticated this user
+		try {
+			String username = session.getAttribute("user_name").toString();
+			if ( ! username.trim().isEmpty() ) {
+				log.info("Already authenticated user " + username);
+				chain.doFilter(request, response);
+				return;
+			}
+		} catch ( Exception ex ) {
+			// Probably null pointer exception - continue on
+			;
+		}
+
+		// Check if already authenticated by Tomcat
 		Principal principal = request.getUserPrincipal();
-		HttpSession session = request.getSession(false);
-
-		if (session==null || principal == null ) {
-			// if custom authentication is used, do something here. If tomcat authentication is used, nothing needs to be done here.
-		}
-		else {
-			// stick the user_name in the session as well so that we don't have to look it up in the database every single time a page is refreshed/updated.
-			String user_name = session.getAttribute("user_name") == null ? null : session.getAttribute("user_name").toString();
-			if(user_name == null || user_name.equals("")){
-				try{
-					String lookup_name = filterConfig.getInitParameter("lookup_name");
-					if(lookup_name!=null && lookup_name.toLowerCase().equals("true")){
-						user_name = lookup(request, principal.getName());
-					}
-					else{
-						user_name=principal.getName();
-					}
-					request.setAttribute("user_name", user_name);
-					session.setAttribute("user_name", user_name);
-					log.info("user " + user_name + " logging in");
-				}
-				catch(Exception e){
-					//request.setAttribute("user_name", "");
-					log.error("Error logging user in: username " + principal.getName() + "; error: " + e.getMessage());
-				}
+		if ( principal != null) {
+			String username = principal.getName();
+			if ( (username != null) && ! username.trim().isEmpty() ) {
+				session.setAttribute("user_name", username);
+				log.info("Tomcat authenticated user " + username);
+				chain.doFilter(request, response);
+				return;
 			}
 		}
 
-		try{
-			// procede if user is authenticated
-			chain.doFilter(request, response);
+		// Check if the username has already been requested
+		String username;
+		try {
+			username = request.getAttribute("user_name").toString();
+			if ( username.trim().isEmpty() )
+				throw new NullPointerException();
+		} catch ( Exception ex ) {
+			// TODO: Make a request for the username, assigning it to attribute "user_name"
+			log.info("TODO: Request username");
+			return;
 		}
-		catch(Exception e){
-			log.error("Error in AuthenticationFilter - " + e.getMessage());
-			// now what?
-		}
-	}
 
-	public void init(FilterConfig filterConfig) throws ServletException
-	{
-		this.filterConfig = filterConfig;
-	}
-
-	public void destroy() { }
-
-	private String lookup(HttpServletRequest request, String name){
-		String user_name = name;
-		try{
-			String db_type = filterConfig.getInitParameter("db_type");
-
-			if(db_type != null){
-				String db_login = filterConfig.getInitParameter("db_login");
-				String db_passwd = filterConfig.getInitParameter("db_passwd");
-				String db_host = filterConfig.getInitParameter("db_host");
-				String db_port = filterConfig.getInitParameter("db_port");
-				String db_name = filterConfig.getInitParameter("db_name");
-				String auth_user_display_name_field = filterConfig.getInitParameter("auth_user_display_name_field");
-				String auth_user_name_field = filterConfig.getInitParameter("auth_user_name_field");
-				String auth_table_name = filterConfig.getInitParameter("auth_table_name");
-
-				Connection con = null;
-
-				Properties connectionProps = new Properties();
-				connectionProps.put("user", db_login);
-				connectionProps.put("password", db_passwd);
-
-				if (db_type.equals("mysql")) {
-					Class.forName("com.mysql.jdbc.Driver");
-					String connectString = "jdbc:" + db_type + "://" + db_host;
-					if(db_port != null && !db_port.equals(""))
-						connectString += ":" + db_port;
-					connectString += "/";
-					con = DriverManager.getConnection(connectString, connectionProps);
-				}
-				// else if(db_type.equals("oracle"){...}
-
-				Statement stmt = null;
-				String query2 =
-					"select " + auth_user_display_name_field + " from " + db_name + "." + auth_table_name + " where " + auth_user_name_field + "='" + name + "'";
-				try {
-					stmt = con.createStatement();
-					ResultSet rs = stmt.executeQuery(query2);
-					while (rs.next()) {
-						name = rs.getString("reviewer");
-						break;
-					}
-				}
-				catch (SQLException e ) {
-					log.error("Error looking up user information - " + e.getMessage());
-				} finally {
-					if (stmt != null) { stmt.close(); }
-					if (con!=null) { con.close();}
-				}
+		int reviewerId = -1;
+		try {
+			Connection catConn = DriverManager.getConnection(databaseUrl, selectUser, selectPass);
+			if ( catConn == null ) {
+				log.error("Unable to make a connection to the database for authentication of " + username);
+				response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "database access failure");
+				return;
 			}
-			else{
-				// add other lookups (flat file, etc.) here as desired
+			PreparedStatement prepStmt = catConn.prepareStatement(
+					"SELECT `reviewer_id` FROM `Reviewers` WHERE `username` = ?");
+			prepStmt.setString(1, username);
+			ResultSet results = prepStmt.executeQuery();
+			while ( results.next() ) {
+				if ( reviewerId > 0 ) {
+					log.error("More than one reviewer matches the username " + username);
+					response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "database configuration errors");
+					return;
+				}
+				reviewerId = results.getInt(1);
 			}
+			results.close();
+			catConn.close();
+		} catch ( SQLException ex ) {
+			log.error("SQLException when trying to authenticate " + username + ": " + ex.getMessage());
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "database access errors");
+			return;
 		}
-		catch(Exception e){
-			log.error("Error looking up user information - " + e.getMessage());
+		if ( reviewerId <= 0 ) {
+			// Unknown username, so return a unauthorized response
+			log.info("Unauthorized: unknown username " + username);
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unknown username");
+			return;
 		}
-		return name;
+		// Set the user_name attribute so subsequent requests in this session can bypass the SQL check
+		session.setAttribute("user_name", username);
+		log.info("Authenticated user " + username);
 	}
+
+	@Override
+	public void destroy() { 
+		; // Nothing to do
+	}
+
 }
