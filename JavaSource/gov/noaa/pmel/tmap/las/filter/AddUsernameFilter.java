28c37a8cb777bc19727b2a0ebcb62d27b59caa3f
Karl Smith <karl.smith@noaa.gov>
2014-06-14 18:52:30 +0000

    Add AddUsernameFilter to add the user_name attribute to sessions which is obtained, if available, from the user Prinicipal name; add log.debug messages to SaveEdits; update DatabaseBackendConfig.xml.in to replace the old SOfCO2gd... databases with SOCATFlags (but with las and lasrules credentials)
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@17749 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ JavaSource/gov/noaa/pmel/tmap/las/filter/AddUsernameFilter.java
@@ -0,0 +1,69 @@
+package gov.noaa.pmel.tmap.las.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+/**
+ * Assigns the "user_name" attribute to a session, which comes from the name
+ * in the user Principal of an authenticated user.
+ */
+public class AddUsernameFilter implements Filter {
+	private static Logger log = LogManager.getLogger(AddUsernameFilter.class.getName());
+	private static final String ATTRIBUTE_NAME = "user_name";
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException { }
+
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, 
+			FilterChain chain) throws IOException, ServletException {
+		log.debug("Entering AddUsernameFilter");
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
+		HttpSession session = request.getSession(false);
+
+		// Check if user_name is already assign for this session 
+		String username;
+		try {
+			username = session.getAttribute(ATTRIBUTE_NAME).toString().trim();
+		} catch ( Exception ex ) {
+			// Probably null pointer exception
+			username = "";
+		}
+
+		if ( username.isEmpty() ) {
+			// Check if there is a username from authenticating
+			try {
+				username = request.getUserPrincipal().getName().trim();
+				if ( ! username.isEmpty() ) {
+					// Add user_name to the session 
+					session.setAttribute(ATTRIBUTE_NAME, username);
+					log.debug("AddUsernameFilter added session attribute " + 
+							ATTRIBUTE_NAME + "=" + username);
+				}
+			} catch ( Exception ex ) {
+				// Probably null pointer exception - nothing to do
+				log.debug("AddUsernameFilter failed to find a username to assign");
+			}
+		}
+
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() { }
+
+}
+
