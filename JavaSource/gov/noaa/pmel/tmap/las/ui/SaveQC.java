06b16008c94c9a1a07cc4aa08191cce46fe070a7
ksmith <ksmith@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2014-08-15 01:50:58 +0000

    Update to fix bugs with DataLocation longitude adjustments and for some reorganization in SocatUploadDashboard.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@18164 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/las/ui/SaveQC.java
+++ JavaSource/gov/noaa/pmel/tmap/las/ui/SaveQC.java
@@ -1,8 +1,8 @@
 package gov.noaa.pmel.tmap.las.ui;
 
-import gov.noaa.pmel.socat.dashboard.nc.DsgNcFileHandler;
+import gov.noaa.pmel.socat.dashboard.handlers.DatabaseRequestHandler;
+import gov.noaa.pmel.socat.dashboard.handlers.DsgNcFileHandler;
 import gov.noaa.pmel.socat.dashboard.server.DashboardServerUtils;
-import gov.noaa.pmel.socat.dashboard.server.DatabaseRequestHandler;
 import gov.noaa.pmel.socat.dashboard.shared.SocatQCEvent;
 import gov.noaa.pmel.tmap.exception.LASException;
 import gov.noaa.pmel.tmap.jdom.LASDocument;
