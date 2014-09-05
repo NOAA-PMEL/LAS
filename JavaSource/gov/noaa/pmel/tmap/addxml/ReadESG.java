14e2c048ae6e13ef8b7fcf85cb9571c5f52d249e
rhs <rhs@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2014-09-05 14:42:55 +0000

    Go back to log4j.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@18323 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/addxml/ReadESG.java
+++ JavaSource/gov/noaa/pmel/tmap/addxml/ReadESG.java
@@ -7,8 +7,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
 
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
+import org.apache.log4j.Logger;
 import org.jdom.Document;
 import org.jdom.Element;
 
@@ -20,7 +19,7 @@ import thredds.catalog.InvDataset;
 import thredds.catalog.ServiceType;
 
 public class ReadESG {
-	private static final Logger log = LoggerFactory.getLogger(ReadESG.class);
+	private static final Logger log = Logger.getLogger(ReadESG.class);
 	public void read(String src) {
 //		addXML ax = new addXML();
 		InvCatalogFactory factory = new InvCatalogFactory("default", false);
