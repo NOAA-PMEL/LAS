669e3767a596223091d45033e8818660e737f2d5
Roland Schweitzer <roland.schweitzer@noaa.gov>
2015-04-08 16:06:14 +0000

    Parse an entire TABLEDAP catalog and put out configuration and categories for all.
    git-svn-id: file:///home/users/tmap/svn/repos/baker/trunk@19416 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- JavaSource/gov/noaa/pmel/tmap/addxml/ErddapScannerOptions.java
+++ JavaSource/gov/noaa/pmel/tmap/addxml/ErddapScannerOptions.java
@@ -6,9 +6,15 @@ import org.apache.commons.cli.Options;
 public class ErddapScannerOptions extends Options {
     
     public ErddapScannerOptions() {
-        Option url = new Option("u", "url", true, "The base URL of the tabledap section of the ERDDAP server (ends in \"/tabledap\"");
+        Option url = new Option("u", "url", true, "The base URL of the tabledap section of the ERDDAP server (ends in \"/tabledap\")");
         url.setRequired(true);
         addOption(url);
+        
+        Option verbose = new Option("v", "verbose", false, "Verbose output while processing.");
+        addOption(verbose);
+        
+        Option title = new Option("t", "title", true, "The title to use for the category for these data. Only used when ID is not specified.");
+        addOption(title);
         
         Option id = new Option("i", "id", true, "The ERDDAP ID of the trajectory data set (see the \"/tabledap\" page on the server.");
         addOption(id);
